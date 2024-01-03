4f37098ec42f7bc75a38d9657bf7f09dbacf6341
man7788 <jerman1841@gmail.com>
2024-01-03 09:50:54 +0800

    Add userController log_in

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -5,7 +5,7 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const Profile = require("../models/profileModel");
 
-// Handle sign-up on POST
+// Handle register on POST
 exports.sign_up = [
   body("username", "Username must not be empty")
     .trim()
@@ -57,6 +57,48 @@ exports.sign_up = [
         } catch (err) {
           return next(err);
         }
+      });
+    }
+  }),
+];
+
+// Handle register on POST
+exports.log_in = [
+  body("username", "Username must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("password", "Password must not be empty")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.json({
+        errors: errors.array(),
+      });
+    } else {
+      const user = await User.findOne({ username: req.body.username });
+      if (!user) {
+        res.json({
+          errors: [{ msg: "User Not Found" }],
+        });
+      }
+
+      const match = await bcrypt.compare(req.body.password, user.password);
+      if (!match) {
+        res.json({
+          errors: [{ msg: "Incorrect Password" }],
+        });
+      }
+
+      jwt.sign({ user }, process.env.JWT_SECRET, (err, token) => {
+        res.json({
+          token,
+        });
       });
     }
   }),
