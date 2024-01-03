8ebbdff4840e923fe5ba336faf97fa3cd1f07d30
man7788 <jerman1841@gmail.com>
2024-01-04 07:10:00 +0800

    Add router post status

--- api/routes/user.js
+++ api/routes/user.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const { verifyToken } = require("../controllers/verifyToken");
 
 const userController = require("../controllers/userController");
+
+// Post request for login status
+router.post("/status", verifyToken, userController.login_status);
 
 module.exports = router;
