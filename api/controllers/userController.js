f7cbd37e9a6756455e1ccaac72e64e2bad141a26
man7788 <jerman1841@gmail.com>
2024-01-04 07:10:19 +0800

    Add login_status controller

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,6 +1,21 @@
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
-const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const User = require("../models/userModel");
 const Profile = require("../models/profileModel");
+
+// Handle check login status on POST
+exports.login_status = [
+  asyncHandler(async (req, res, next) => {
+    jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
+      if (err) {
+        res.sendStatus(401);
+      } else {
+        const profile = await Profile.findById(authData.user.profile);
+        res.status(200).json({
+          profile,
+        });
+      }
+    });
+  }),
+];
