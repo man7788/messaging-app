cd9114d05bb54419b0c60b5193340029b6715fbb
man7788 <jerman1841@gmail.com>
2024-01-03 08:36:08 +0800

    Update userController sign_up

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,6 +1,7 @@
 const { body, validationResult } = require("express-validator");
-const User = require("../models/userModel");
 const asyncHandler = require("express-async-handler");
+const User = require("../models/userModel");
+const Profile = require("../models/profileModel");
 
 // Handle sign-up on POST
 exports.sign_up = [
@@ -23,9 +24,18 @@ exports.sign_up = [
       return value === req.body.password;
     })
     .withMessage("Passwords do not match"),
+  body("name", "Name must not be empty").trim().isLength({ min: 1 }).escape(),
 
   asyncHandler(async (req, res, next) => {
     const errors = validationResult(req);
+
+    const profile = new Profile({ name: req.body.name });
+
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+      profile,
+    });
 
     if (!errors.isEmpty()) {
       res.json({
@@ -35,11 +45,6 @@ exports.sign_up = [
         errors: errors.array(),
       });
     } else {
-      const user = new User({
-        username: req.body.username,
-        password: req.body.password,
-      });
-
       const result = await user.save();
       res.json(result);
     }
