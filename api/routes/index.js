197de9443b4e4f5358d5a686d06fda29e6889f81
man7788 <jerman1841@gmail.com>
2024-01-04 02:50:02 +0800

    Move login and sign-up route to index

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-/* GET home page. */
+const userController = require("../controllers/userController");
+
+// Get request for homepage
 router.get("/", function (req, res, next) {
   res.render("index", { title: "Express" });
 });
+
+// Post request for user sign-up
+router.post("/signup", userController.sign_up);
+
+// Post request for user user log-in
+router.post("/login", userController.log_in);
 
 module.exports = router;
