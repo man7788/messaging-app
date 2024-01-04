125c9920a48ae85acf15b776e28c2ed7945268a3
man7788 <jerman1841@gmail.com>
2024-01-05 05:03:14 +0800

    Add login function

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,11 +1,73 @@
 import styles from "./Login.module.css";
+import { useState } from "react";
+import { LoginFetch } from "./fetch/LoginFetch";
 
-function Login() {
+const Login = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [serverError, setServerError] = useState(false);
+  const [formErrors, setFormErrors] = useState([]);
+
+  const [loading, setLoading] = useState(false);
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const loginPayload = { username, password };
+
+    LoginFetch(loginPayload, setFormErrors, setServerError, setLoading);
+  };
+
+  if (serverError) {
+    return (
+      <div>
+        <h1>A network error was encountered</h1>
+      </div>
+    );
+  }
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.Login}>
-      <h1>Login</h1>
+      <h1>Messaging App</h1>
+      <h2>Login</h2>
+      <form action="" method="post" onSubmit={onSubmitForm}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        ></input>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        ></input>
+        <button type="submit">Log In</button>
+      </form>
+      {formErrors && (
+        <ul>
+          {formErrors.map((error) => (
+            <li key={error.msg}>{error.msg}</li>
+          ))}
+        </ul>
+      )}
     </div>
   );
-}
+};
 
 export default Login;
