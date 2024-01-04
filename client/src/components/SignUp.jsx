8aeb05b540ef0c09cc2dcea03623ff9cbe1febe6
man7788 <jerman1841@gmail.com>
2024-01-05 04:39:46 +0800

    Add SignUp component

---
+++ client/src/components/SignUp.jsx
@@ -0,0 +1,11 @@
+import styles from "./SignUp.module.css";
+
+function SignUp() {
+  return (
+    <div className={styles.SignUp}>
+      <h1>SignUp</h1>
+    </div>
+  );
+}
+
+export default SignUp;
