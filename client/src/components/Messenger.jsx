65270efab17669a02639f155875c91a8747748b8
man7788 <jerman1841@gmail.com>
2024-01-05 04:39:33 +0800

    Add Messenger component

---
+++ client/src/components/Messenger.jsx
@@ -0,0 +1,11 @@
+import styles from "./Messenger.module.css";
+
+function Messenger() {
+  return (
+    <div className={styles.Messenger}>
+      <h1>Messenger</h1>
+    </div>
+  );
+}
+
+export default Messenger;
