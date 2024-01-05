5694253aaf164ed2220db1dcfe40474cbb81ad55
man7788 <jerman1841@gmail.com>
2024-01-05 08:23:11 +0800

    Update LoginFetch

--- client/src/fetch/LoginFetch.jsx
+++ client/src/fetch/LoginFetch.jsx
@@ -2,33 +2,31 @@ const LoginFetch = (
   loginPayload,
   setFormErrors,
   setServerError,
-  setLoading
+  setLoading,
 ) => {
   fetch(`http://localhost:3000/login`, {
-    mode: "cors",
-    method: "POST",
+    mode: 'cors',
+    method: 'POST',
     headers: {
-      "Content-Type": "application/json",
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify(loginPayload),
   })
     .then((response) => {
       console.log(response);
       if (response.status >= 400) {
-        throw new Error("server error");
+        throw new Error('server error');
       }
       return response.json();
     })
     .then((response) => {
       if (response && response.errors) {
         setFormErrors(response.errors);
-        throw new Error("login error");
+        throw new Error('login error');
       }
-      console.log("Success:", response);
-      localStorage.setItem("token", JSON.stringify(response.token));
     })
     .catch((error) => {
-      if (error && error.message == "sever error") {
+      if (error && error.message == 'sever error') {
         setServerError(error);
       }
 
