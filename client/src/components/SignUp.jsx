894f3be9bbe819ec95291c56ef3faed8d59d0757
man7788 <jerman1841@gmail.com>
2024-01-06 07:41:18 +0800

    Add navigate to log in

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,9 +1,15 @@
-import styles from "./SignUp.module.css";
+import styles from './SignUp.module.css';
+import { useState } from 'react';
+import { Navigate } from 'react-router-dom';
 
 function SignUp() {
+  const [loginRedirect, setLoginRedirect] = useState(null);
   return (
     <div className={styles.SignUp}>
-      <h1>SignUp</h1>
+      <h1>Messaging App</h1>
+      <h2>SignUp</h2>
+      <button onClick={() => setLoginRedirect(true)}>Log In</button>
+      {loginRedirect && <Navigate to="/login" />}
     </div>
   );
 }
