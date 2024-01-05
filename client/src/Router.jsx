f6fd5c01e85a1542110ef011f0a763cfc5289dfc
man7788 <jerman1841@gmail.com>
2024-01-05 08:23:57 +0800

    Add routes

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,12 +1,30 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import App from "./App";
 import ErrorPage from "./ErrorPage";
+import Messenger from "./components/messenger";
+import Login from "./components/login";
+import SignUp from "./components/Signup";
 
 const Router = () => {
   const router = createBrowserRouter([
     {
       path: "/",
       element: <App />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/messenger",
+      element: <Messenger />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
+      errorElement: <ErrorPage />,
+    },
+    {
+      path: "/signup",
+      element: <SignUp />,
       errorElement: <ErrorPage />,
     },
   ]);
