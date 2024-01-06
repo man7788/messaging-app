3d343637fc6c5f791bee99b25c856b0695719843
man7788 <jerman1841@gmail.com>
2024-01-06 10:06:01 +0800

    Add sign up form

--- client/src/components/SignUp.jsx
+++ client/src/components/SignUp.jsx
@@ -1,17 +1,87 @@
 import styles from './SignUp.module.css';
 import { useState } from 'react';
 import { Navigate } from 'react-router-dom';
+import SignUpFetch from '../fetch/SignUpAPI';
 
-function SignUp() {
+const SignUp = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const [serverError, setServerError] = useState(null);
+  const [formErrors, setFormErrors] = useState([]);
+
+  const [loading, setLoading] = useState(null);
+
   const [loginRedirect, setLoginRedirect] = useState(null);
+
+  const onSubmitForm = (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const signUpPayload = { username, password, confirmPassword };
+
+    SignUpFetch(signUpPayload, setFormErrors, setServerError, setLoading);
+  };
+
+  if (serverError) {
+    return (
+      <div>
+        <h1>A network error was encountered</h1>
+      </div>
+    );
+  }
+
+  if (loading) {
+    return (
+      <div>
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.SignUp}>
       <h1>Messaging App</h1>
-      <h2>SignUp</h2>
+      <h2>Sign Up</h2>
+      <form action="" method="post" onSubmit={onSubmitForm}>
+        <label htmlFor="username">Username:</label>
+        <input
+          type="text"
+          name="username"
+          id="username"
+          value={username}
+          onChange={(event) => setUsername(event.target.value)}
+        ></input>
+        <label htmlFor="password">Password:</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          value={password}
+          onChange={(event) => setPassword(event.target.value)}
+        ></input>
+        <label htmlFor="confirmPassword">Confirm Password:</label>
+        <input
+          type="password"
+          name="confirmPassword"
+          id="confirmPassword"
+          value={confirmPassword}
+          onChange={(event) => setConfirmPassword(event.target.value)}
+        ></input>
+        <button type="submit">Sign Up</button>
+      </form>
       <button onClick={() => setLoginRedirect(true)}>Log In</button>
+      {formErrors && (
+        <ul>
+          {formErrors.map((error) => (
+            <li key={error.msg}>{error.msg}</li>
+          ))}
+        </ul>
+      )}
       {loginRedirect && <Navigate to="/login" />}
     </div>
   );
-}
+};
 
 export default SignUp;
