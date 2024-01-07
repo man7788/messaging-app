ab5b60439238a19f0a9ad3b236d9081c734a1c2d
man7788 <jerman1841@gmail.com>
2024-01-07 08:16:40 +0800

    Format prettier

--- client/src/Router.jsx
+++ client/src/Router.jsx
@@ -1,29 +1,29 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import App from "./App";
-import ErrorPage from "./ErrorPage";
-import Messenger from "./components/messenger";
-import Login from "./components/login";
-import SignUp from "./components/Signup";
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import App from './App';
+import ErrorPage from './ErrorPage';
+import Messenger from './components/messenger';
+import Login from './components/login';
+import SignUp from './components/Signup';
 
 const Router = () => {
   const router = createBrowserRouter([
     {
-      path: "/",
+      path: '/',
       element: <App />,
       errorElement: <ErrorPage />,
     },
     {
-      path: "/messenger",
+      path: '/messenger',
       element: <Messenger />,
       errorElement: <ErrorPage />,
     },
     {
-      path: "/login",
+      path: '/login',
       element: <Login />,
       errorElement: <ErrorPage />,
     },
     {
-      path: "/signup",
+      path: '/signup',
       element: <SignUp />,
       errorElement: <ErrorPage />,
     },
