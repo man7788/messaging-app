f5e53738707fa403550506327a9e0b9cc76b5b51
man7788 <jerman1841@gmail.com>
2024-01-10 08:09:12 +0800

    Add edit profile route

--- api/routes/user.js
+++ api/routes/user.js
@@ -7,4 +7,7 @@ const userController = require("../controllers/userController");
 // Post request for login status
 router.post("/status", verifyToken, userController.login_status);
 
+// Post request for edit profile
+router.post("/edit", verifyToken, userController.edit_profile);
+
 module.exports = router;
