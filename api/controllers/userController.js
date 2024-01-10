7b675761d020581aa5e86968e498ed44f08c08f1
man7788 <jerman1841@gmail.com>
2024-01-10 08:08:59 +0800

    Add edit profile controller

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,7 +1,6 @@
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
-const User = require("../models/userModel");
 const Profile = require("../models/profileModel");
 
 // Handle check login status on POST
@@ -17,5 +16,46 @@ exports.login_status = [
         });
       }
     });
+  }),
+];
+
+// Handle edit profile status on POST
+exports.edit_profile = [
+  body("id", "Id must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("name", "Name must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("about")
+    .trim()
+    .isLength({ max: 150 })
+    .withMessage("About should contain less than 150 words")
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.json({
+        errors: errors.array(),
+      });
+    } else {
+      jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
+        if (err) {
+          res.json({ error: "invalid token" });
+        } else {
+          const profile = new Profile({
+            name: req.body.name,
+            about: req.body.about,
+            _id: req.body.id,
+          });
+          const updatedProfile = await Profile.findByIdAndUpdate(
+            req.body.id,
+            profile
+          );
+          res.json({
+            profile,
+            updatedProfile,
+          });
+        }
+      });
+    }
   }),
 ];
