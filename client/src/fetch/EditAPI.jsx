39946ee0b655d736603f68364faa8a6bffc09326
man7788 <jerman1841@gmail.com>
2024-01-11 05:53:43 +0800

    Add EditAPI

---
+++ client/src/fetch/EditAPI.jsx
@@ -0,0 +1,31 @@
+const EditFetch = async (editPayload) => {
+  const token = JSON.parse(localStorage.getItem('token'));
+  try {
+    const response = await fetch(`http://localhost:3000/user/edit`, {
+      mode: 'cors',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(editPayload),
+    });
+
+    if (response.status >= 400) {
+      throw new Error('server error');
+    }
+
+    const responseData = await response.json();
+
+    if (responseData && responseData.errors) {
+      return { formErrors: responseData.errors };
+    }
+    console.log(responseData);
+    return { responseData };
+  } catch (error) {
+    console.error(error);
+    return { error: error.message };
+  }
+};
+
+export default EditFetch;
