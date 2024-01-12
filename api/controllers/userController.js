496a9ce9ea83f5a8ab53a90fb51fec94f575f0d9
man7788 <jerman1841@gmail.com>
2024-01-13 06:05:35 +0800

    Update edit profile

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -1,18 +1,20 @@
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Profile = require("../models/profileModel");
+const User = require("../models/userModel");
 
 // Handle check login status on POST
-exports.login_status = [
+exports.user_status = [
   asyncHandler(async (req, res, next) => {
     jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
       if (err) {
         res.json({ error: "invalid token" });
       } else {
-        const profile = await Profile.findById(authData.user.profile);
+        const user = await User.findById(authData.user._id).populate("profile");
         res.json({
-          profile,
+          user,
         });
       }
     });
@@ -21,9 +23,22 @@ exports.login_status = [
 
 // Handle edit profile status on POST
 exports.edit_profile = [
-  body("id", "Id must not be empty").trim().isLength({ min: 1 }).escape(),
-  body("name", "Name must not be empty").trim().isLength({ min: 1 }).escape(),
-  body("about")
+  body("user_id", "Id must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("new_username", "Username must not be empty")
+    .trim()
+    .isLength({ min: 1, max: 200 })
+    .escape()
+    .custom(async (value, { req }) => {
+      const user = await User.findOne({ username: value });
+      if (user && user._id.valueOf() !== req.body.user_id) {
+        throw new Error("Username already in use");
+      }
+    }),
+  body("new_name", "Name must not be empty")
+    .trim()
+    .isLength({ min: 1, max: 200 })
+    .escape(),
+  body("new_about")
     .trim()
     .isLength({ max: 150 })
     .withMessage("About should contain less than 150 words")
@@ -41,18 +56,105 @@ exports.edit_profile = [
         if (err) {
           res.json({ error: "invalid token" });
         } else {
+          const userDoc = await User.findById(req.body.user_id).populate(
+            "profile"
+          );
+
           const profile = new Profile({
-            name: req.body.name,
-            about: req.body.about,
-            _id: req.body.id,
+            name: req.body.new_name,
+            about: req.body.new_about,
+            _id: userDoc.profile._id,
           });
+
+          const user = new User({
+            username: req.body.new_username,
+            password: userDoc.password,
+            profile: userDoc.profile._id,
+            friends: userDoc.friends,
+            _id: userDoc._id,
+          });
+
           const updatedProfile = await Profile.findByIdAndUpdate(
-            req.body.id,
+            userDoc.profile._id,
             profile
           );
+
+          const updatedUser = await User.findByIdAndUpdate(
+            req.body.user_id,
+            user
+          );
+
           res.json({
-            profile,
-            updatedProfile,
+            updated_user: user,
+            previous_user: updatedUser,
+            updated_profile: profile,
+            previous_profile: updatedProfile,
+          });
+        }
+      });
+    }
+  }),
+];
+
+// Handle change password on POST
+exports.edit_password = [
+  body("user_id", "Id must not be empty").trim().isLength({ min: 1 }).escape(),
+  body("current_password", "Current password must not be empty")
+    .trim()
+    .isLength({ min: 1, max: 200 })
+    .escape()
+    .custom(async (value, { req }) => {
+      const user = await User.findById(req.body.user_id, "password");
+      const match = await bcrypt.compare(value, user.password);
+      if (!match) {
+        throw new Error("Incorrect current Password");
+      }
+    }),
+  body("new_password", "Password must not be empty")
+    .trim()
+    .isLength({ min: 8, max: 200 })
+    .withMessage("Password at least contains 8 characters")
+    .escape(),
+  body("confirm_new_password")
+    .trim()
+    .custom((value, { req }) => {
+      return value === req.body.new_password;
+    })
+    .withMessage("Passwords do not match")
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.json({
+        errors: errors.array(),
+      });
+    } else {
+      jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
+        if (err) {
+          res.json({ error: "invalid token" });
+        } else {
+          const userDoc = await User.findById(req.body.user_id);
+
+          const hashedPassword = bcrypt.hashSync(req.body.new_password, 10);
+
+          const user = new User({
+            username: userDoc.username,
+            password: hashedPassword,
+            profile: userDoc.profile,
+            friends: userDoc.friends,
+            _id: userDoc._id,
+          });
+
+          const updatedUser = await User.findByIdAndUpdate(
+            req.body.user_id,
+            user
+          );
+
+          res.json({
+            updated_user: user,
+            previous_user: updatedUser,
           });
         }
       });
