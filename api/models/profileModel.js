35b17f7ebc4ac7e86fc6cd6f6ced96cba113334e
man7788 <jerman1841@gmail.com>
2024-01-14 07:50:21 +0800

    Update Profile model

--- api/models/profileModel.js
+++ api/models/profileModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ProfileSchema = new Schema({
-  name: { type: String, minLength: 1, maxLength: 50 },
+  full_name: { type: String, minLength: 1, maxLength: 100 },
   about: { type: String, minLength: 1, maxLength: 2000 },
 });
 
