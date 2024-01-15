7d6b88a8d821f89136f3ca1a90792cb5a2130477
man7788 <jerman1841@gmail.com>
2024-01-16 07:36:45 +0800

    Add UserAPI

---
+++ client/src/fetch/UserAPI.jsx
@@ -0,0 +1,44 @@
+import { useEffect, useState } from 'react';
+
+const useProfiles = () => {
+  const [profiles, setProfiles] = useState(null);
+  const [profilesLoading, setProfilesLoading] = useState(true);
+  const [profilesError, setProfilesError] = useState(null);
+
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('token'));
+    const fetchProfiles = async () => {
+      if (token !== undefined) {
+        try {
+          const response = await fetch(`http://localhost:3000/user/profiles`, {
+            mode: 'cors',
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          if (response.status >= 400) {
+            throw new Error('server error');
+          }
+          const responseData = await response.json();
+
+          if (responseData && responseData.error) {
+            setProfiles(responseData);
+          } else if (responseData && responseData.profiles) {
+            setProfiles(responseData.profiles);
+          }
+        } catch (error) {
+          setProfilesError(error.message);
+        } finally {
+          setProfilesLoading(false);
+        }
+      }
+    };
+    fetchProfiles();
+  }, []);
+
+  return { profiles, profilesLoading, profilesError };
+};
+
+export default useProfiles;
