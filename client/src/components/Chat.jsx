2852a14b65e3450bb24b5f9155a84ef76cd6629e
man7788 <jerman1841@gmail.com>
2024-01-16 10:58:01 +0800

    Add condition render when no message

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,12 +1,67 @@
 import styles from './Chat.module.css';
-import { useContext } from 'react';
+import { useContext, useState } from 'react';
 import { chatContext } from '../contexts/chatContext';
 
 const Chat = () => {
   const { chatProfile } = useContext(chatContext);
+  const [messages, setMessages] = useState(null);
+  const [outMessage, setOutMessage] = useState('');
+
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+    // setLoading(true);
+
+    // const editPayload = {
+    //   new_full_name: fullName,
+    //   new_about: about,
+    //   profile_id: profileId,
+    // };
+
+    // const result = await EditFetch(editPayload);
+
+    // if (result && result.error) {
+    //   setServerError(true);
+    // }
+
+    // if (result && result.formErrors) {
+    //   setFormErrors(result.formErrors);
+    // }
+
+    // setLoading(false);
+
+    // if (result && result.responseData) {
+    //   setSuccess(true);
+    //   setFormErrors(null);
+    // }
+  };
 
   return (
-    <div className={styles.Chat}>{chatProfile && chatProfile.full_name}</div>
+    <>
+      {chatProfile ? (
+        <div className={styles.Chat}>
+          <div className={styles.ChatTitle}>
+            {chatProfile && chatProfile.full_name}
+          </div>
+          <div className={styles.messages}>
+            {messages ? 'some messages' : 'There is no message'}
+          </div>
+          <div className={styles.input}>
+            <form action="" method="post" onSubmit={onSubmitForm}>
+              <input
+                type="text"
+                name="messageOut"
+                id="new_messageOutfull_name"
+                value={outMessage}
+                onChange={(event) => setOutMessage(event.target.value)}
+              ></input>
+              <button type="submit">Send</button>
+            </form>
+          </div>
+        </div>
+      ) : (
+        <div className={styles.Chat}>No chats selected</div>
+      )}
+    </>
   );
 };
 
