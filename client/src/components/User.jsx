037ca591c9bf2ef3858932e3a55c53efe1896fae
man7788 <jerman1841@gmail.com>
2024-01-16 08:53:33 +0800

    Add button onclick to update context

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -1,9 +1,17 @@
 import styles from './User.module.css';
+import { useContext } from 'react';
+import { chatContext } from '../contexts/chatContext';
 
 const User = ({ profile }) => {
+  const { setChatProfile } = useContext(chatContext);
+
+  const onChangeChat = () => {
+    setChatProfile(profile);
+  };
+
   return (
     <div className={styles.User}>
-      <button>{profile.full_name}</button>
+      <button onClick={onChangeChat}>{profile.full_name}</button>
     </div>
   );
 };
