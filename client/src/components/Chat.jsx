c320fa3e87e38a104317c87b41adcd0f3b306692
man7788 <jerman1841@gmail.com>
2024-01-16 08:54:21 +0800

    Add useContext to consume chatProfile

--- client/src/components/Chat.jsx
+++ client/src/components/Chat.jsx
@@ -1,7 +1,13 @@
 import styles from './Chat.module.css';
+import { useContext } from 'react';
+import { chatContext } from '../contexts/chatContext';
 
 const Chat = () => {
-  return <div className={styles.Chat}>placeholder</div>;
+  const { chatProfile } = useContext(chatContext);
+
+  return (
+    <div className={styles.Chat}>{chatProfile && chatProfile.full_name}</div>
+  );
 };
 
 export default Chat;
