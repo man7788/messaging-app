b41e508d090d07ff30dcf11b84e6e90ac68c7be6
man7788 <jerman1841@gmail.com>
2024-01-18 04:00:43 +0800

    Update use authData user._id

--- api/controllers/chatController.js
+++ api/controllers/chatController.js
@@ -1,11 +1,55 @@
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
-const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
-const User = require("../models/userModel");
-const Profile = require("../models/profileModel");
 const Chat = require("../models/chatModel");
 const Message = require("../models/messageModel");
+
+// Handle request for chat messages on GET
+// exports.chat_messages = [
+//   asyncHandler(async (req, res, next) => {
+//     const errors = validationResult(req);
+
+//     if (!errors.isEmpty()) {
+//       res.json({
+//         errors: errors.array(),
+//       });
+//     } else {
+//       jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
+//         if (err) {
+//           res.json({ error: "invalid token" });
+//         } else {
+//           let messages;
+//           const chat = await Chat.find({
+//             users: { $in: [authData.user._id, req.body._user_id] },
+//           });
+
+//           messages = await messages.find({ _id: chat._id });
+
+//           // if (chatData) {
+//           // } else {
+//           //   const newChat = new Chat({
+//           //     users: [req.body.user_id, req.body._user_id],
+//           //   });
+
+//           //   chat = await newChat.save();
+//           // }
+
+//           // const message = new Message({
+//           //   chat: chat._id,
+//           //   text: req.body.message,
+//           //   date: new Date(),
+//           //   author: req.body.user_id,
+//           // });
+
+//           // const createdMessage = await message.save();
+
+//           // res.json({ chat, createdMessage });
+//           res.json({ chat });
+//         }
+//       });
+//     }
+//   }),
+// ];
 
 // Handle send message on POST
 exports.send_message = [
@@ -22,32 +66,38 @@ exports.send_message = [
         errors: errors.array(),
       });
     } else {
-      let chat;
+      jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
+        if (err) {
+          res.json({ error: "invalid token" });
+        } else {
+          let chat;
 
-      const chatData = await Chat.findOne({
-        users: { $in: [req.body.user_id, req.body._user_id] },
+          const chatData = await Chat.findOne({
+            users: { $in: [authData.user._id, req.body.user_id] },
+          });
+
+          if (chatData) {
+            chat = chatData;
+          } else {
+            const newChat = new Chat({
+              users: [authData.user._id, req.body.user_id],
+            });
+
+            chat = await newChat.save();
+          }
+
+          const message = new Message({
+            chat: chat._id,
+            text: req.body.message,
+            date: new Date(),
+            author: req.body.user_id,
+          });
+
+          const createdMessage = await message.save();
+
+          res.json({ chat, createdMessage });
+        }
       });
-
-      if (chatData) {
-        chat = chatData;
-      } else {
-        const newChat = new Chat({
-          users: [req.body.user_id, req.body._user_id],
-        });
-
-        chat = await newChat.save();
-      }
-
-      const message = new Message({
-        chat: chat._id,
-        text: req.body.message,
-        date: new Date(),
-        author: req.body.user_id,
-      });
-
-      const createdMessage = await message.save();
-
-      res.json({ chat, createdMessage });
     }
   }),
 ];
