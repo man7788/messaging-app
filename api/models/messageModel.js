95355014dba44d009279e00434ccfd84839746f8
man7788 <jerman1841@gmail.com>
2024-01-17 08:48:03 +0800

    Add messageModel

---
+++ api/models/messageModel.js
@@ -0,0 +1,17 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const MessageSchema = new Schema({
+  text: {
+    type: String,
+    required: true,
+    minLength: 1,
+    maxLength: 10000,
+  },
+  date: { type: Date, default: new Date() },
+  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+// Export model
+module.exports = mongoose.model("Message", MessageSchema);
