8d2d3594b9e886c753d7cb92e6e2485b8b694827
man7788 <jerman1841@gmail.com>
2024-01-17 10:57:59 +0800

    Add verifyToken to send message route

--- api/routes/chat.js
+++ api/routes/chat.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
+const { verifyToken } = require("../controllers/verifyToken");
 
 const chatController = require("../controllers/chatController");
 
 // Post request for send message
-router.post("/send", chatController.send_message);
+router.post("/send", verifyToken, chatController.send_message);
 
 module.exports = router;
