20396bf4d36b05753e4d63ac2e57d5e27ce014c0
man7788 <jerman1841@gmail.com>
2024-01-18 08:48:34 +0800

    Change method to post

--- api/routes/chat.js
+++ api/routes/chat.js
@@ -4,6 +4,9 @@ const { verifyToken } = require("../controllers/verifyToken");
 
 const chatController = require("../controllers/chatController");
 
+// Post request for chat messages
+router.post("/messages", verifyToken, chatController.chat_messages);
+
 // Post request for send message
 router.post("/send", verifyToken, chatController.send_message);
 
