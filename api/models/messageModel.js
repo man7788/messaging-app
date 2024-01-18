5d0d6eb68060748a5e51a604ab4579f58f993a3f
man7788 <jerman1841@gmail.com>
2024-01-19 04:27:50 +0800

    Add virtual formatted date

--- api/models/messageModel.js
+++ api/models/messageModel.js
@@ -1,17 +1,28 @@
+const { DateTime } = require("luxon");
+
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const MessageSchema = new Schema({
-  chat: { type: Schema.Types.ObjectId, ref: "Chat", required: true },
-  text: {
-    type: String,
-    required: true,
-    minLength: 1,
-    maxLength: 10000,
+const MessageSchema = new Schema(
+  {
+    chat: { type: Schema.Types.ObjectId, ref: "Chat", required: true },
+    text: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 10000,
+    },
+    date: { type: Date, default: new Date() },
+    author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   },
-  date: { type: Date, default: new Date() },
-  author: { type: Schema.Types.ObjectId, ref: "User", required: true },
+  { toJSON: { virtuals: true } }
+);
+
+MessageSchema.virtual("date_med_with_seconds").get(function () {
+  return DateTime.fromJSDate(this.date).toLocaleString(
+    DateTime.DATETIME_MED_WITH_SECONDS
+  );
 });
 
 // Export model
