ddd03bd0b44156eb3975ded3046eafb832dd3e71
man7788 <jerman1841@gmail.com>
2024-01-18 08:49:31 +0800

    Add chat_messages handler

--- api/controllers/chatController.js
+++ api/controllers/chatController.js
@@ -4,52 +4,35 @@ const jwt = require("jsonwebtoken");
 const Chat = require("../models/chatModel");
 const Message = require("../models/messageModel");
 
-// Handle request for chat messages on GET
-// exports.chat_messages = [
-//   asyncHandler(async (req, res, next) => {
-//     const errors = validationResult(req);
+// Handle request for chat messages on POST
+exports.chat_messages = [
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
 
-//     if (!errors.isEmpty()) {
-//       res.json({
-//         errors: errors.array(),
-//       });
-//     } else {
-//       jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
-//         if (err) {
-//           res.json({ error: "invalid token" });
-//         } else {
-//           let messages;
-//           const chat = await Chat.find({
-//             users: { $in: [authData.user._id, req.body._user_id] },
-//           });
+    if (!errors.isEmpty()) {
+      res.json({
+        errors: errors.array(),
+      });
+    } else {
+      jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
+        if (err) {
+          res.json({ error: "invalid token" });
+        } else {
+          const chat = await Chat.findOne({
+            users: { $all: [authData.user._id, req.body.user_id] },
+          });
 
-//           messages = await messages.find({ _id: chat._id });
-
-//           // if (chatData) {
-//           // } else {
-//           //   const newChat = new Chat({
-//           //     users: [req.body.user_id, req.body._user_id],
-//           //   });
-
-//           //   chat = await newChat.save();
-//           // }
-
-//           // const message = new Message({
-//           //   chat: chat._id,
-//           //   text: req.body.message,
-//           //   date: new Date(),
-//           //   author: req.body.user_id,
-//           // });
-
-//           // const createdMessage = await message.save();
-
-//           // res.json({ chat, createdMessage });
-//           res.json({ chat });
-//         }
-//       });
-//     }
-//   }),
-// ];
+          if (chat) {
+            const messages = await Message.find({ chat: chat._id });
+            res.json({ messages });
+          } else {
+            res.json({ messages: null });
+          }
+        }
+      });
+    }
+  }),
+];
 
 // Handle send message on POST
 exports.send_message = [
