8b6857ed644e7a81f24a13756a62118ec4a85fcc
man7788 <jerman1841@gmail.com>
2024-01-19 07:54:54 +0800

    Update setTextStyle for message style

--- client/src/components/Text.jsx
+++ client/src/components/Text.jsx
@@ -2,13 +2,16 @@ import styles from './Text.module.css';
 import { useEffect, useState } from 'react';
 
 const Text = ({ message, loginId }) => {
-  const [textStyle, setTextStyle] = useState('sent');
+  const [textStyle, setTextStyle] = useState('');
 
   useEffect(() => {
     if (message.author !== loginId) {
       setTextStyle('received');
+    } else {
+      setTextStyle('sent');
     }
   }, []);
+
   return (
     <div className={styles.Text}>
       <li key={message._id} className={styles[textStyle]}>
