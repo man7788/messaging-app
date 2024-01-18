259b69792982fc57ebbd94e031f993d7e834eaa0
man7788 <jerman1841@gmail.com>
2024-01-18 09:57:14 +0800

    Add text style render

--- client/src/components/Text.jsx
+++ client/src/components/Text.jsx
@@ -1,9 +1,19 @@
 import styles from './Text.module.css';
+import { useEffect, useState } from 'react';
 
-const Text = ({ message }) => {
+const Text = ({ message, loginId }) => {
+  const [textStyle, setTextStyle] = useState('sent');
+
+  useEffect(() => {
+    if (message.author !== loginId) {
+      setTextStyle('recevied');
+    }
+  }, []);
   return (
     <div className={styles.Text}>
-      <li key={message._id}>{message.text}</li>
+      <li key={message._id} className={styles[textStyle]}>
+        {message.text}
+      </li>
     </div>
   );
 };
