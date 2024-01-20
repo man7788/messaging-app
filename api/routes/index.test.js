812153ff8e453717dfd1c416c3d210defb91a3b1
man7788 <jerman1841@gmail.com>
2024-01-21 03:18:57 +0800

    Add log-in route test

--- api/routes/index.test.js
+++ api/routes/index.test.js
@@ -1,10 +1,13 @@
-const index = require("./index");
 const { connectDB, dropDB, dropCollections } = require("../setuptestdb");
-const bcrypt = require("bcryptjs");
-
 const request = require("supertest");
 const express = require("express");
 const app = express();
+
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+
+const index = require("./index");
+const User = require("../models/userModel");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -21,6 +24,16 @@ afterAll(async () => {
 afterEach(async () => {
   await dropCollections();
 });
+
+afterEach(() => {
+  jest.clearAllMocks();
+});
+
+jest.mock("jsonwebtoken", () => ({
+  sign: jest.fn((token, secretOrPublicKey, options, callback) => {
+    return callback(null, "123abc$");
+  }),
+}));
 
 describe("index routes", () => {
   test("index sign-up route responses with created user", async () => {
@@ -45,5 +58,32 @@ describe("index routes", () => {
     expect(response.body.email).toBe("john@doe.com");
     expect(response.body.password).not.toBe("johndoefoobar");
     expect(response.body.profile.full_name).toBe("foobar");
+  });
+
+  test("index log-in route responses with token", async () => {
+    const hash = bcrypt.hashSync("johndoefoobar", 10);
+
+    const user = new User({
+      email: "john@doe.com",
+      profile: new mongoose.Types.ObjectId(),
+      password: hash,
+    });
+
+    await user.save();
+
+    const payload = {
+      email: "john@doe.com",
+      password: "johndoefoobar",
+    };
+
+    const response = await request(app)
+      .post("/login")
+      .set("Content-Type", "application/json")
+      .send(payload);
+
+    expect(response.header["content-type"]).toMatch(/application\/json/);
+    expect(response.status).toEqual(200);
+
+    expect(response.body.token).toBe("123abc$");
   });
 });
