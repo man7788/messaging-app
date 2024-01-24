6c0769baa0896279da78e84cf19a5ab178039de5
man7788 <jerman1841@gmail.com>
2024-01-24 10:32:48 +0800

    Refactor mocks for all tests

--- api/routes/index.test.js
+++ api/routes/index.test.js
@@ -1,38 +1,29 @@
 const request = require("supertest");
 const express = require("express");
-const app = express();
-
-const mongoose = require("mongoose");
-
 const index = require("./index");
-const User = require("../models/userModel");
-const Profile = require("../models/profileModel");
-const { errorMonitor } = require("supertest/lib/test");
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", index);
 
-afterEach(() => {
-  jest.clearAllMocks();
-});
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
-jest.mock("jsonwebtoken", () => ({
-  sign: jest.fn((token, secretOrPublicKey, options, callback) => {
-    return callback(null, "123abc$");
-  }),
-}));
-
-jest.mock("bcryptjs", () => ({
-  hash: jest.fn((password, salt, callback) => {
-    return callback(null, "hashedpassword");
-  }),
-  compare: jest.fn(() => true),
-}));
-
+const User = require("../models/userModel");
+const Profile = require("../models/profileModel");
 const userFindOneSpy = jest.spyOn(User, "findOne");
 const userSaveSpy = jest.spyOn(User.prototype, "save");
 const profileSaveSpy = jest.spyOn(Profile.prototype, "save");
+
+afterEach(() => {
+  jest.clearAllMocks();
+  jest.resetAllMocks();
+});
+
+jest.mock("bcryptjs");
+jest.mock("jsonwebtoken");
 
 const profile_id = new mongoose.Types.ObjectId();
 const user_id = new mongoose.Types.ObjectId();
@@ -44,6 +35,10 @@ describe("index routes", () => {
     });
 
     test("successful sign-up responses with created user", async () => {
+      bcrypt.hash.mockImplementationOnce((password, salt, callback) => {
+        return callback(null, "hashedpassword");
+      });
+
       profileSaveSpy.mockResolvedValueOnce({
         full_name: "foobar",
         _id: profile_id,
@@ -102,22 +97,30 @@ describe("index routes", () => {
     });
   });
 
-  test("index log-in route responses with token", async () => {
-    userFindOneSpy.mockResolvedValueOnce(true);
+  describe("log-in controller", () => {
+    test("index log-in route responses with token", async () => {
+      userFindOneSpy.mockResolvedValueOnce(true);
+      bcrypt.compare.mockImplementationOnce(() => true);
+      jwt.sign.mockImplementationOnce(
+        (token, secretOrPublicKey, options, callback) => {
+          return callback(null, "123abc$");
+        }
+      );
 
-    const payload = {
-      email: "john@doe.com",
-      password: "johndoefoobar",
-    };
+      const payload = {
+        email: "john@doe.com",
+        password: "johndoefoobar",
+      };
 
-    const response = await request(app)
-      .post("/login")
-      .set("Content-Type", "application/json")
-      .send(payload);
+      const response = await request(app)
+        .post("/login")
+        .set("Content-Type", "application/json")
+        .send(payload);
 
-    expect(response.header["content-type"]).toMatch(/application\/json/);
-    expect(response.status).toEqual(200);
+      expect(response.header["content-type"]).toMatch(/application\/json/);
+      expect(response.status).toEqual(200);
 
-    expect(response.body.token).toBe("123abc$");
+      expect(response.body.token).toBe("123abc$");
+    });
   });
 });
