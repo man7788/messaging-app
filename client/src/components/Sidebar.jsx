174fb51d07f045e216aa60498eb8759c8f09d00e
man7788 <jerman1841@gmail.com>
2024-01-30 07:23:02 +0800

    Add setting button

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -1,29 +1,34 @@
 import styles from './Sidebar.module.css';
+import { useState } from 'react';
 import useProfiles from '../fetch/UserAPI';
 import Setting from './Setting';
 import UserList from './UserList';
 
 const Sidebar = ({ name, loginId }) => {
   const { profiles, profilesLoading, profilesError } = useProfiles();
+  const [showSetting, setShowSetting] = useState(null);
+
+  const onShowSetting = () => {
+    if (!showSetting) {
+      setShowSetting(!showSetting);
+    } else if (showSetting) {
+      setShowSetting(!showSetting);
+    }
+  };
 
   return (
     <div className={styles.Sidebar}>
-      <div className={styles.sidebarContainer}>
-        <div className={styles.userInfo}>
-          {name}
-          <div className={styles.setting}>
-            <Setting />
-          </div>
-        </div>
-        <div className={styles.userList}>
-          <UserList
-            loginId={loginId}
-            profiles={profiles}
-            profilesLoading={profilesLoading}
-            profilesError={profilesError}
-          />
-        </div>
+      <div className={styles.userInfo}>
+        {name}
+        <button onClick={onShowSetting}>Setting</button>
+        {showSetting && <Setting />}
       </div>
+      <UserList
+        loginId={loginId}
+        profiles={profiles}
+        profilesLoading={profilesLoading}
+        profilesError={profilesError}
+      />
     </div>
   );
 };
