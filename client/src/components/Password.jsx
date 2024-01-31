11e74f612446343b8479092b3098738e513f32d1
man7788 <jerman1841@gmail.com>
2024-01-31 08:11:19 +0800

    Add Password render containers

--- client/src/components/Password.jsx
+++ client/src/components/Password.jsx
@@ -11,6 +11,9 @@ const Password = () => {
   const [currentPassword, setCurrentPassword] = useState('');
   const [password, setPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
+  const [currentPasswordError, setCurrentPasswordError] = useState(null);
+  const [passwordError, setPasswordError] = useState(null);
+  const [confirmPasswordError, setConfirmPasswordError] = useState(null);
 
   const [loading, setLoading] = useState(true);
   const [serverError, setServerError] = useState(null);
@@ -39,9 +42,26 @@ const Password = () => {
     setLoading(false);
   }, [status]);
 
+  useEffect(() => {
+    if (formErrors) {
+      for (const error of formErrors) {
+        if (/current/i.test(error.msg)) {
+          setCurrentPasswordError(error.msg);
+        } else if (/\bcharacters/i.test(error.msg)) {
+          setPasswordError(error.msg);
+        } else if (/\bmatch$/i.test(error.msg)) {
+          setConfirmPasswordError(error.msg);
+        }
+      }
+    }
+  }, [formErrors]);
+
   const onSubmitForm = async (e) => {
     e.preventDefault();
     setLoading(true);
+    setCurrentPasswordError(null);
+    setPasswordError(null);
+    setConfirmPasswordError(null);
 
     const passwordPayload = {
       user_id: userId,
@@ -70,6 +90,9 @@ const Password = () => {
     if (result && result.responseData) {
       setSuccess(true);
       setFormErrors(null);
+      setCurrentPassword('');
+      setPassword('');
+      setConfirmPassword('');
     }
 
     setLoading(false);
@@ -92,46 +115,65 @@ const Password = () => {
   }
 
   return (
-    <div className={styles.Password}>
-      <h1>Messaging App</h1>
-      <h2>Edit</h2>
-      <form action="" method="post" onSubmit={onSubmitForm}>
-        <label htmlFor="current_password">Current Password:</label>
-        <input
-          type="password"
-          name="current_password"
-          id="current_password"
-          value={currentPassword}
-          onChange={(event) => setCurrentPassword(event.target.value)}
-        ></input>
-        <label htmlFor="new_password">New Password:</label>
-        <input
-          type="password"
-          name="new_password"
-          id="new_password"
-          value={password}
-          onChange={(event) => setPassword(event.target.value)}
-        ></input>
-        <label htmlFor="confirm_new_password">Confirm New Password:</label>
-        <input
-          type="password"
-          name="confirm_new_password"
-          id="confirm_new_password"
-          value={confirmPassword}
-          onChange={(event) => setConfirmPassword(event.target.value)}
-        ></input>
-        <button type="submit">Save</button>
-      </form>
-      <button onClick={() => setAppRedirect(true)}>Back</button>
+    <div className={styles.PasswordContainer}>
+      <div className={styles.heading}>
+        <h2>Change Password</h2>
+      </div>
+      <div className={styles.Password}>
+        <div className={styles.formContainer}>
+          <form action="" method="post" onSubmit={onSubmitForm}>
+            <div className={styles.inputContainer}>
+              <label htmlFor="current_password">Current Password:</label>
+              <input
+                className={currentPasswordError ? styles.inputOutline : null}
+                type="password"
+                name="current_password"
+                id="current_password"
+                value={currentPassword}
+                onChange={(event) => setCurrentPassword(event.target.value)}
+              ></input>
+              <div className={styles.inputError}>{currentPasswordError}</div>
+            </div>
+            <div className={styles.inputContainer}>
+              <label htmlFor="new_password">New Password:</label>
+              <input
+                className={passwordError ? styles.inputOutline : null}
+                type="password"
+                name="new_password"
+                id="new_password"
+                value={password}
+                onChange={(event) => setPassword(event.target.value)}
+              ></input>
+              <div className={styles.inputError}>{passwordError}</div>
+            </div>
+            <div className={styles.inputContainer}>
+              <label htmlFor="confirm_new_password">
+                Confirm New Password:
+              </label>
+              <input
+                className={confirmPasswordError ? styles.inputOutline : null}
+                type="password"
+                name="confirm_new_password"
+                id="confirm_new_password"
+                value={confirmPassword}
+                onChange={(event) => setConfirmPassword(event.target.value)}
+              ></input>
+              <div className={styles.inputError}>{confirmPasswordError}</div>
+            </div>
+            <div className={styles.success}>
+              {success && 'Password successfully updated'}
+            </div>
+            <div className={styles.saveBtn}>
+              <button type="submit">Save</button>
+            </div>
+            <hr></hr>
+            <div className={styles.backBtn}>
+              <button onClick={() => setAppRedirect(true)}>Back</button>
+            </div>
+          </form>
+        </div>
+      </div>
       {appRedirect && <Navigate to="/" />}
-      {formErrors && (
-        <ul>
-          {formErrors.map((error) => (
-            <li key={error.msg}>{error.msg}</li>
-          ))}
-        </ul>
-      )}
-      {success && 'Password successfully updated'}
     </div>
   );
 };
