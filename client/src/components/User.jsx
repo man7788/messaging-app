2c1ed88382313f6a277d8e3134fa4699a637967a
man7788 <jerman1841@gmail.com>
2024-01-31 10:31:54 +0800

    Add User button style condition

--- client/src/components/User.jsx
+++ client/src/components/User.jsx
@@ -1,9 +1,19 @@
 import styles from './User.module.css';
-import { useContext } from 'react';
+import { useContext, useEffect, useState } from 'react';
 import { chatContext } from '../contexts/chatContext';
 
 const User = ({ profile }) => {
-  const { setChatProfile } = useContext(chatContext);
+  const { setChatProfile, chatProfile } = useContext(chatContext);
+  const [activeProfile, setActiveProfile] = useState(null);
+
+  useEffect(() => {
+    console.log(chatProfile, profile.full_name);
+    if (chatProfile && chatProfile.full_name === profile.full_name) {
+      setActiveProfile(!activeProfile);
+    } else {
+      setActiveProfile(false);
+    }
+  }, [chatProfile]);
 
   const onChangeChat = () => {
     setChatProfile(profile);
@@ -11,7 +21,12 @@ const User = ({ profile }) => {
 
   return (
     <div className={styles.User}>
-      <button onClick={onChangeChat}>{profile.full_name}</button>
+      <div
+        className={activeProfile ? styles.buttonActive : styles.buttonDiv}
+        onClick={onChangeChat}
+      >
+        <button>{profile.full_name}</button>
+      </div>
     </div>
   );
 };
