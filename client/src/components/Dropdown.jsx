89f6c7406fcc0dec3b3b7ee3b519b0028ac4d696
man7788 <jerman1841@gmail.com>
2024-02-07 04:03:33 +0800

    Change Setting to Dropdown

--- client/src/components/Dropdown.jsx
+++ client/src/components/Dropdown.jsx
@@ -2,20 +2,21 @@ import styles from './Dropdown.module.css';
 import { useState } from 'react';
 import { Navigate } from 'react-router-dom';
 
-const Setting = ({ setShowUserList }) => {
-  const [appRedirect, setAppRedirect] = useState(null);
+const Dropdown = ({ setShowUserList }) => {
+  const [appRedirect, setAppRedirect] = useState(false);
 
   const onSettings = () => {
     setShowUserList(false);
   };
 
-  const onLogOut = () => {
+  const onLogOut = (e) => {
+    e.stopPropagation();
     localStorage.clear();
     setAppRedirect(true);
   };
 
   return (
-    <div className={styles.Setting}>
+    <div className={styles.Dropdown}>
       <div className={styles.button} onClick={onSettings}>
         <button>Settings</button>
       </div>
@@ -23,10 +24,9 @@ const Setting = ({ setShowUserList }) => {
       <div className={styles.button} onClick={onLogOut}>
         <button>Log Out</button>
       </div>
-
       {appRedirect && <Navigate to="/" replace={true} />}
     </div>
   );
 };
 
-export default Setting;
+export default Dropdown;
