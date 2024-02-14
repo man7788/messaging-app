9411a6073c37b4c18bbf16a56590ca90c1c57379
man7788 <jerman1841@gmail.com>
2024-02-15 02:54:50 +0800

    Add navigates to signup test

--- client/src/components/Login.test.jsx
+++ client/src/components/Login.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Login from './Login';
 import * as useStatus from '../fetch/StatusAPI';
 
@@ -12,48 +13,70 @@ vi.mock('react-router-dom', () => ({
 
 const useStatusSpy = vi.spyOn(useStatus, 'default');
 
-describe('Login', () => {
-  describe('render from useStatus result', () => {
-    test('should render error page', async () => {
-      useStatusSpy.mockReturnValue({
-        result: null,
-        loading: false,
-        serverError: true,
-      });
-
-      render(<Login />);
-
-      const errorDiv = screen.getByTestId('error');
-
-      expect(errorDiv).toBeInTheDocument();
+describe('from useStatus result', () => {
+  test('should render error page', async () => {
+    useStatusSpy.mockReturnValue({
+      result: null,
+      loading: false,
+      serverError: true,
     });
 
-    test('should render loading page', async () => {
-      useStatusSpy.mockReturnValue({
-        result: null,
-        loading: true,
-        serverError: null,
-      });
+    render(<Login />);
 
-      render(<Login />);
+    const errorDiv = screen.getByTestId('error');
 
-      const loadingDiv = screen.getByTestId('loading');
+    expect(errorDiv).toBeInTheDocument();
+  });
 
-      expect(loadingDiv).toBeInTheDocument();
+  test('should render loading page', () => {
+    useStatusSpy.mockReturnValue({
+      result: null,
+      loading: true,
+      serverError: null,
     });
 
-    test('should render login page', async () => {
-      useStatusSpy.mockReturnValue({
-        result: { error: 'token error message' },
-        loading: false,
-        serverError: null,
-      });
+    render(<Login />);
 
-      render(<Login />);
+    const loadingDiv = screen.getByTestId('loading');
 
-      const login = screen.getByRole('button', { name: /log in/i });
+    expect(loadingDiv).toBeInTheDocument();
+  });
 
-      expect(login).toBeInTheDocument();
+  test('should render login page', () => {
+    useStatusSpy.mockReturnValue({
+      result: { error: 'token error message' },
+      loading: false,
+      serverError: null,
     });
+
+    render(<Login />);
+
+    const login = screen.getByRole('button', { name: /log in/i });
+
+    expect(login).toBeInTheDocument();
+  });
+});
+
+describe('singup button', () => {
+  test('should navigate to singup page', async () => {
+    const user = userEvent.setup();
+
+    useStatusSpy.mockReturnValue({
+      result: { error: 'token error message' },
+      loading: false,
+      serverError: null,
+    });
+
+    render(<Login />);
+
+    const signup = screen.getByRole('button', { name: /sign up/i });
+
+    await user.click(signup);
+
+    const LoginDiv = screen.getByText(/redirected/i);
+
+    expect(LoginDiv.childNodes[1].textContent).toMatch(
+      /Redirected to \/signup/i,
+    );
   });
 });
