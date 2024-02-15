8e504afa2197cc813ca69e254258e48e52760963
man7788 <jerman1841@gmail.com>
2024-02-16 04:01:51 +0800

    Add user input values test

--- client/src/components/SignUp.test.jsx
+++ client/src/components/SignUp.test.jsx
@@ -47,6 +47,32 @@ describe('signup form', () => {
 
     expect(loadingDiv).toBeInTheDocument();
   });
+
+  test('should show user input values', async () => {
+    const user = userEvent.setup();
+
+    render(<SignUp />);
+
+    const email = screen.getByTestId('email');
+    const fullName = screen.getByTestId('full_name');
+    const password = screen.getByTestId('password');
+    const confirmPassword = screen.getByTestId('confirm_password');
+
+    await user.type(email.childNodes[0], 'foo@foobar.com');
+    await user.type(fullName.childNodes[0], 'foobar');
+    await user.type(password.childNodes[0], 'password123');
+    await user.type(confirmPassword.childNodes[0], 'password123');
+
+    const emailValue = await screen.findByTestId('email');
+    const fullNameValue = await screen.findByTestId('full_name');
+    const passwordValue = await screen.findByTestId('password');
+    const confirmPasswordValue = await screen.findByTestId('confirm_password');
+
+    expect(emailValue.childNodes[0].value).toMatch(/foo@foobar.com/i);
+    expect(fullNameValue.childNodes[0].value).toMatch(/foobar/i);
+    expect(passwordValue.childNodes[0].value).toMatch(/password123/i);
+    expect(confirmPasswordValue.childNodes[0].value).toMatch(/password123/i);
+  });
 });
 
 describe('cancel button', () => {
