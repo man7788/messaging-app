a290e85620d08713df4e06d07d94c527aa18432b
man7788 <jerman1841@gmail.com>
2024-02-22 10:41:51 +0800

    Add submit form test

--- client/src/components/Messenger/Content/Edit.test.jsx
+++ client/src/components/Messenger/Content/Edit.test.jsx
@@ -2,8 +2,10 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import Edit from './Edit';
 import * as useStatus from '../../../fetch/StatusAPI';
+import * as EditFetch from '../../../fetch/EditAPI';
 
 const useStatusSpy = vi.spyOn(useStatus, 'default');
+const editFetchSpy = vi.spyOn(EditFetch, 'default');
 
 describe('from useStatus result', () => {
   test('should render error page', async () => {
@@ -52,14 +54,16 @@ describe('from useStatus result', () => {
 });
 
 describe('Edit form', () => {
-  test('should display user input value', async () => {
-    const user = userEvent.setup();
-
+  beforeEach(() => {
     useStatusSpy.mockReturnValue({
       result: { profile: { full_name: 'foobar', about: 'first child' } },
       loading: false,
       serverError: null,
     });
+  });
+
+  test('should display user input value', async () => {
+    const user = userEvent.setup();
 
     render(<Edit />);
 
@@ -68,6 +72,30 @@ describe('Edit form', () => {
 
     await user.type(fullName, ' 1st');
     await user.type(about, ' 1st');
+
+    expect(fullName.value).toMatch(/foobar 1st/i);
+    expect(about.value).toMatch(/first child 1st/i);
+  });
+
+  test('should submit user form with new values', async () => {
+    const user = userEvent.setup();
+    const newFullName = ' 1st';
+    const newAbout = ' 1st';
+
+    editFetchSpy.mockReturnValue({
+      result: { updated_profile: { full_name: newFullName, about: newAbout } },
+    });
+
+    render(<Edit />);
+
+    const fullName = screen.getByLabelText(/full name/i);
+    const about = screen.getByLabelText(/about/i);
+
+    await user.type(fullName, newFullName);
+    await user.type(about, newAbout);
+
+    const submit = await screen.findByRole('button');
+    await user.click(submit);
 
     expect(fullName.value).toMatch(/foobar 1st/i);
     expect(about.value).toMatch(/first child 1st/i);
