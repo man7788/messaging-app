f39281968228743852bf9b169f62eaccda81864d
man7788 <jerman1841@gmail.com>
2024-02-24 07:23:10 +0800

    Add render error page test

---
+++ client/src/components/Messenger/Content/Password.test.jsx
@@ -0,0 +1,29 @@
+import { render, screen } from '@testing-library/react';
+import Password from './Password';
+import * as useStatus from '../../../fetch/StatusAPI';
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
+
+const useStatusSpy = vi.spyOn(useStatus, 'default');
+
+vi.mock('react-router-dom', () => ({
+  Navigate: vi.fn(({ to }) => `Redirected to ${to}`),
+}));
+
+describe('from useStatus result', () => {
+  test('should render error page', async () => {
+    useStatusSpy.mockReturnValue({
+      result: null,
+      loading: false,
+      serverError: true,
+    });
+
+    render(<Password />);
+
+    const error = screen.getByTestId('error');
+
+    expect(error).toBeInTheDocument();
+  });
+});
