01e7a69f0cf598b1a70ace3a54582717c3201e9e
man7788 <jerman1841@gmail.com>
2024-02-24 08:34:06 +0800

    Add show user input value test

--- client/src/components/Messenger/Content/Password.test.jsx
+++ client/src/components/Messenger/Content/Password.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import Password from './Password';
 import * as useStatus from '../../../fetch/StatusAPI';
 
@@ -39,5 +40,35 @@ describe('from useStatus result', () => {
     const loading = screen.getByTestId('loading');
 
     expect(loading).toBeInTheDocument();
+  });
+});
+
+describe('Password form', () => {
+  beforeEach(() => {
+    useStatusSpy.mockReturnValue({
+      result: {
+        profile: { full_name: 'foobar', about: 'first child', _id: '1001' },
+      },
+      loading: false,
+      serverError: null,
+    });
+  });
+
+  test('should display user input value', async () => {
+    const user = userEvent.setup();
+
+    render(<Password />);
+
+    const currentPassword = screen.getByLabelText(/current password/i);
+    const newPassword = screen.getByLabelText(/^new password/i);
+    const ConfirmNewPassword = screen.getByLabelText(/confirm new password/i);
+
+    await user.type(currentPassword, 'oldpassword');
+    await user.type(newPassword, 'newpassword');
+    await user.type(ConfirmNewPassword, ' newpassword');
+
+    expect(currentPassword.value).toMatch(/oldpassword/i);
+    expect(newPassword.value).toMatch(/newpassword/i);
+    expect(ConfirmNewPassword.value).toMatch(/newpassword/i);
   });
 });
