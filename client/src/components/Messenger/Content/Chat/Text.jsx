d3323a5aaf765c738ded84fcad7733bf4fe30edd
man7788 <jerman1841@gmail.com>
2024-03-03 03:37:43 +0800

    Add render message image

--- client/src/components/Messenger/Content/Chat/Text.jsx
+++ client/src/components/Messenger/Content/Chat/Text.jsx
@@ -4,10 +4,19 @@ import { useEffect, useState, useRef } from 'react';
 const Text = ({ message, loginId }) => {
   const bottom = useRef();
   const [textStyle, setTextStyle] = useState('');
+  const [image, setImage] = useState('');
+
+  const toBase64 = (uInt8Array) => btoa(String.fromCharCode(...uInt8Array));
 
   useEffect(() => {
     bottom.current?.scrollIntoView();
   });
+
+  useEffect(() => {
+    message &&
+      message.image &&
+      setImage(`data:image/jpeg;base64,${toBase64(message.image.data.data)}`);
+  }, []);
 
   useEffect(() => {
     if (message.author !== loginId) {
@@ -21,7 +30,12 @@ const Text = ({ message, loginId }) => {
     <div className={styles.Text} ref={bottom} data-testid="text">
       <div key={message._id} className={styles[textStyle]}>
         <div className={styles.textContent}>
-          <div className={styles.messageText}>{message.text}</div>
+          {message && message.text && (
+            <div className={styles.messageText}>{message.text}</div>
+          )}
+          {message && message.image && (
+            <img className={styles.messageText} src={image} />
+          )}
           <div className={styles.time}>{message.time_simple}</div>
         </div>
       </div>
