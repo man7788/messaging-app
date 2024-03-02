b70c4a7409ada11ae9119a00df44a46b763f5218
man7788 <jerman1841@gmail.com>
2024-03-03 03:01:11 +0800

    Update message model

--- api/models/messageModel.js
+++ api/models/messageModel.js
@@ -9,9 +9,11 @@ const MessageSchema = new Schema(
     chat: { type: Schema.Types.ObjectId, ref: "Chat", required: true },
     text: {
       type: String,
-      required: true,
       minLength: 1,
       maxLength: 10000,
+    },
+    image: {
+      data: Buffer,
     },
     date: { type: Date, default: new Date() },
     author: { type: Schema.Types.ObjectId, ref: "User", required: true },
