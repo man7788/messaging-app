051343e52fd3866d7421fee3a5379cd782cb9f92
man7788 <jerman1841@gmail.com>
2024-03-02 09:27:24 +0800

    Add send image router

--- api/routes/chat.js
+++ api/routes/chat.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const { verifyToken } = require("../controllers/verifyToken");
+const upload = require("../controllers/multerConfig");
 
 const chatController = require("../controllers/chatController");
 
@@ -9,5 +10,13 @@ router.post("/messages", verifyToken, chatController.chat_messages);
 
 // Post request for send message
 router.post("/send", verifyToken, chatController.send_message);
+
+// Post request for send image
+router.post(
+  "/send/image",
+  verifyToken,
+  upload.single("msgImage"),
+  chatController.send_image
+);
 
 module.exports = router;
