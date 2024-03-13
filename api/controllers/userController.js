b6d6a8c51d9cb38039c35b8379736a8ad9c38b2e
man7788 <jerman1841@gmail.com>
2024-03-14 02:08:00 +0800

    Add friend controllers

--- api/controllers/userController.js
+++ api/controllers/userController.js
@@ -4,6 +4,24 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const Profile = require("../models/profileModel");
 const User = require("../models/userModel");
+const Request = require("../models/requestModel");
+const Friend = require("../models/friendModel");
+
+// Handle check login status on POST
+exports.user_status = [
+  asyncHandler(async (req, res, next) => {
+    jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
+      if (err) {
+        res.json({ error: "invalid token" });
+      } else {
+        const user = await User.findById(authData.user._id).populate("profile");
+        res.json({
+          user,
+        });
+      }
+    });
+  }),
+];
 
 // Handle request for all profiles on GET
 exports.profiles = [
@@ -30,17 +48,114 @@ exports.profiles = [
   }),
 ];
 
-// Handle check login status on POST
-exports.user_status = [
+// Handle request for create friend request on POST
+exports.request_friend = [
   asyncHandler(async (req, res, next) => {
     jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
       if (err) {
         res.json({ error: "invalid token" });
       } else {
-        const user = await User.findById(authData.user._id).populate("profile");
-        res.json({
-          user,
+        const friend = await Friend.findOne({
+          users: { $in: [req.body.user_id] },
         });
+
+        if (friend) {
+          res.json(null);
+        } else {
+          const request = new Request({
+            from: authData.user._id,
+            to: req.body.user_id,
+          });
+
+          const createdRequest = await request.save();
+
+          res.json({ createdRequest });
+        }
+      }
+    });
+  }),
+];
+
+// Handle request for all friend requests on Get
+exports.requests = [
+  asyncHandler(async (req, res, next) => {
+    jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
+      if (err) {
+        res.json({ error: "invalid token" });
+      } else {
+        const requests = await Request.find({
+          to: authData.user._id,
+        });
+
+        res.json({ requests });
+      }
+    });
+  }),
+];
+
+// Handle request for add friend on POST
+exports.add_friend = [
+  asyncHandler(async (req, res, next) => {
+    jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
+      if (err) {
+        res.json({ error: "invalid token" });
+      } else {
+        const isFriend = await Friend.findOne({
+          users: { $all: [authData.user._id, req.body.user_id] },
+        });
+
+        if (isFriend) {
+          res.json(null);
+        } else {
+          const friend = new Friend({
+            users: [authData.user._id, req.body.user_id],
+          });
+
+          const createdFriend = await friend.save();
+
+          res.json({ createdFriend });
+        }
+      }
+    });
+  }),
+];
+
+// Handle request for all friends on GET
+exports.friends = [
+  asyncHandler(async (req, res, next) => {
+    jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
+      if (err) {
+        res.json({ error: "invalid token" });
+      } else {
+        const profiles = [];
+
+        const friends = await Friend.find({
+          users: { $in: [authData.user._id] },
+        });
+
+        if (friends) {
+          for (const friend of friends) {
+            const friendId = friend.users.filter(
+              (id) => id.toString() !== authData.user._id
+            );
+
+            const profile = await User.findOne(
+              { _id: friendId[0] },
+              "profile"
+            ).populate("profile");
+
+            profiles.push({
+              user_id: profile._id,
+              _id: profile.profile._id,
+              full_name: profile.full_name,
+              about: profile.about,
+            });
+          }
+
+          res.json({ profiles });
+        } else {
+          res.json({ friends: null });
+        }
       }
     });
   }),
