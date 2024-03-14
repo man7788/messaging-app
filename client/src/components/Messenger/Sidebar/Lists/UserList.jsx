e51d174bf3c5dffe3d9109c96c458bcec7f0e6b6
man7788 <jerman1841@gmail.com>
2024-03-15 01:09:08 +0800

    Add render non-friends

--- client/src/components/Messenger/Sidebar/Lists/UserList.jsx
+++ client/src/components/Messenger/Sidebar/Lists/UserList.jsx
@@ -2,22 +2,62 @@ import styles from './UserList.module.css';
 import User from './User';
 import { useEffect, useRef, useState } from 'react';
 
-const UserList = ({ loginId, profiles, profilesLoading, profilesError }) => {
+const UserList = ({ loginId, profileProps, friendProps }) => {
   const listRef = useRef();
+  const { profiles, profilesLoading, profilesError } = profileProps;
+  const { friends, friendsLoading, friendssError } = friendProps;
+  const [notFriends, setNotFriends] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
   const [renderList, setRenderList] = useState(null);
   const [isOverFlow, setIsOverFlow] = useState(null);
 
   useEffect(() => {
-    if (profiles && profiles.length > 0) {
+    if (profilesError || friendssError) {
+      setError(true);
+    }
+  }, [profilesError, friendssError]);
+
+  useEffect(() => {
+    if (!profilesLoading && !friendsLoading) {
+      setLoading(false);
+    }
+  }, [profilesLoading, friendsLoading]);
+
+  useEffect(() => {
+    const list = [];
+
+    if (profiles && friends) {
+      for (const profile of profiles) {
+        const isFriend = [];
+
+        for (const friend of friends) {
+          if (profile.user_id === friend.user_id) {
+            isFriend.push('is friend');
+          }
+        }
+
+        if (isFriend.length === 0) {
+          list.push(profile);
+        }
+      }
+    }
+
+    setNotFriends(list);
+  }, [profiles, friends]);
+
+  useEffect(() => {
+    if (notFriends && notFriends.length > 0) {
       setRenderList(true);
     }
 
     if (listRef.current?.scrollHeight > listRef.current?.clientHeight) {
       setIsOverFlow(true);
     }
-  }, [profiles]);
+  }, [notFriends]);
 
-  if (profilesError) {
+  if (error) {
     return (
       <div className={styles.error} data-testid="error">
         <h1>A network error was encountered</h1>
@@ -25,7 +65,7 @@ const UserList = ({ loginId, profiles, profilesLoading, profilesError }) => {
     );
   }
 
-  if (profilesLoading) {
+  if (loading) {
     return (
       <div className={styles.loading} data-testid="loading">
         <div className={styles.loader}></div>
@@ -39,7 +79,7 @@ const UserList = ({ loginId, profiles, profilesLoading, profilesError }) => {
       ref={listRef}
     >
       {renderList &&
-        profiles.map((profile) => {
+        notFriends.map((profile) => {
           if (profile.user_id !== loginId) {
             return <User key={profile._id} profile={profile} />;
           }
