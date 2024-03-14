9c029571d0fe4cb108c54a5b2f1108fa00f77610
man7788 <jerman1841@gmail.com>
2024-03-14 08:57:42 +0800

    Add show friend list

--- client/src/components/Messenger/Sidebar/Sidebar.jsx
+++ client/src/components/Messenger/Sidebar/Sidebar.jsx
@@ -1,8 +1,10 @@
 import styles from './Sidebar.module.css';
 import { useContext, useState } from 'react';
+import useFriends from '../../../fetch/useFriendsAPI';
 import useProfiles from '../../../fetch/UserAPI';
 import Dropdown from './Dropdown';
 import UserList from './Lists/UserList';
+import FriendList from './Lists/FriendList';
 import hamburger from '../../../images/hamburger.svg';
 import arrow from '../../../images/arrow.svg';
 import avatar from '../../../images/avatar.svg';
@@ -11,17 +13,28 @@ import SettingList from './Lists/SettingList';
 
 const Sidebar = ({ name, loginId, showHamburger }) => {
   const { profiles, profilesLoading, profilesError } = useProfiles();
+  const { friends, friendsLoading, friendssError } = useFriends();
   const { setContentArea } = useContext(chatContext);
-  const [showUserList, setShowUserList] = useState(true);
+  const [showFriendList, setShowFriendList] = useState(true);
+  const [showUserList, setShowUserList] = useState(false);
+  const [showSettings, setShowSettings] = useState(false);
 
-  const onShowUser = () => {
+  const onShowFriends = () => {
+    setShowFriendList(true);
+    setShowUserList(false);
+    setShowSettings(false);
+    setContentArea('chat');
+  };
+
+  const onShowUsers = () => {
     setShowUserList(true);
+    setShowFriendList(false);
     setContentArea('chat');
   };
 
   return (
     <div>
-      {showUserList ? (
+      {!showSettings && (
         <div className={styles.Sidebar} data-testid="sidebar">
           <div className={styles.userInfo}>
             <div className={styles.loginUser}>
@@ -30,6 +43,12 @@ const Sidebar = ({ name, loginId, showHamburger }) => {
               </div>
               {name}
             </div>
+            {showFriendList ? (
+              <button onClick={onShowUsers}>Add</button>
+            ) : (
+              <button onClick={onShowFriends}>Friends</button>
+            )}
+
             <button
               id="hamburger"
               className={showHamburger ? styles.buttonActive : null}
@@ -37,19 +56,37 @@ const Sidebar = ({ name, loginId, showHamburger }) => {
             >
               <img id="hamburger" src={hamburger}></img>
             </button>
-            {showHamburger && <Dropdown setShowUserList={setShowUserList} />}
+            {showHamburger && (
+              <Dropdown
+                setShowUserList={setShowUserList}
+                setShowFriendList={setShowFriendList}
+                setShowSettings={setShowSettings}
+              />
+            )}
           </div>
-          <UserList
-            loginId={loginId}
-            profiles={profiles}
-            profilesLoading={profilesLoading}
-            profilesError={profilesError}
-          />
+          {showUserList && !showSettings && (
+            <UserList
+              loginId={loginId}
+              profiles={profiles}
+              profilesLoading={profilesLoading}
+              profilesError={profilesError}
+            />
+          )}
+          {showFriendList && (
+            <FriendList
+              loginId={loginId}
+              friends={friends}
+              friendsLoading={friendsLoading}
+              friendssError={friendssError}
+            />
+          )}
         </div>
-      ) : (
+      )}
+
+      {showSettings && (
         <div className={styles.Sidebar}>
           <div className={styles.settingInfo}>
-            <button onClick={onShowUser}>
+            <button onClick={onShowFriends}>
               <img src={arrow}></img>
             </button>
             <div>Settings</div>
