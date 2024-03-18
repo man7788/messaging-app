973d9146c979eb07fc144bca5da537f5a3236313
man7788 <jerman1841@gmail.com>
2024-03-19 01:17:30 +0800

    Update User component

--- client/src/components/Messenger/Sidebar/Lists/User.jsx
+++ client/src/components/Messenger/Sidebar/Lists/User.jsx
@@ -1,37 +1,56 @@
 import styles from './User.module.css';
-import { useContext, useEffect, useState } from 'react';
-import { chatContext } from '../../../../contexts/chatContext';
+import { useState } from 'react';
+import RequestCreateFetch from '../../../../fetch/RequestCreateAPI';
 import avatar from '../../../../images/avatar.svg';
 
-const User = ({ profile }) => {
-  const { setChatProfile, chatProfile } = useContext(chatContext);
-  const [activeProfile, setActiveProfile] = useState(null);
+const User = ({ profile, sent }) => {
+  const [sentRequest, setSentRequest] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
 
-  useEffect(() => {
-    if (chatProfile && chatProfile.full_name === profile.full_name) {
-      setActiveProfile(!activeProfile);
-    } else {
-      setActiveProfile(false);
+  const onSendRequest = async () => {
+    setLoading(true);
+
+    let requestPayload = { user_id: profile.user_id };
+
+    const result = await RequestCreateFetch(requestPayload);
+
+    if (result && result.error) {
+      setError(true);
     }
-  }, [chatProfile]);
 
-  const onChangeChat = () => {
-    setChatProfile(profile);
+    if (result && result.responseData) {
+      setSentRequest(true);
+      setLoading(false);
+    }
   };
+
+  if (error) {
+    return (
+      <div className={styles.error} data-testid="error">
+        <h1>A network error was encountered</h1>
+      </div>
+    );
+  }
+
+  if (loading) {
+    return (
+      <div className={styles.loading} data-testid="loading">
+        <div className={styles.loader}></div>
+      </div>
+    );
+  }
 
   return (
     <div className={styles.User}>
-      <div
-        className={activeProfile ? styles.buttonActive : styles.buttonDiv}
-        onClick={onChangeChat}
-      >
-        <button>
-          <div className={styles.avatarContainer}>
-            <img src={avatar}></img>
-          </div>
-          {profile.full_name}
-        </button>
+      <div className={styles.avatarContainer}>
+        <img src={avatar}></img>
       </div>
+      {profile.full_name}
+      {!sent && !sentRequest && (
+        <button onClick={onSendRequest}>Add Friend</button>
+      )}
+      {(sent || sentRequest) && <div>Request sent</div>}
     </div>
   );
 };
