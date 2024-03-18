2330a3631573b261cf586cd19a59f4907700c327
man7788 <jerman1841@gmail.com>
2024-03-19 01:16:53 +0800

    Add RequestCreateAPI

---
+++ client/src/fetch/RequestCreateAPI.jsx
@@ -0,0 +1,31 @@
+const RequestCreateFetch = async (sendPayload) => {
+  const token = JSON.parse(localStorage.getItem('token'));
+  try {
+    const response = await fetch(
+      `http://localhost:3000/user/friend/request/create`,
+      {
+        mode: 'cors',
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${token}`,
+        },
+        body: JSON.stringify(sendPayload),
+      },
+    );
+
+    if (response.status >= 400) {
+      throw new Error('server error');
+    }
+
+    const responseData = await response.json();
+
+    console.log(responseData);
+    return { responseData };
+  } catch (error) {
+    console.error(error);
+    return { error: error.message };
+  }
+};
+
+export default RequestCreateFetch;
