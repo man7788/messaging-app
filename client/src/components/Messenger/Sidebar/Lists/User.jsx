bc3c61577c118adb0ed4aaa8aca58e8d091347be
man7788 <jerman1841@gmail.com>
2024-03-21 03:23:53 +0800

    Add accept friend button

--- client/src/components/Messenger/Sidebar/Lists/User.jsx
+++ client/src/components/Messenger/Sidebar/Lists/User.jsx
@@ -1,10 +1,12 @@
 import styles from './User.module.css';
 import { useState } from 'react';
 import RequestCreateFetch from '../../../../fetch/RequestCreateAPI';
+import FriendFetch from '../../../../fetch/FriendAPI';
 import avatar from '../../../../images/avatar.svg';
 
-const User = ({ profile, sent }) => {
+const User = ({ profile, sent, received }) => {
   const [sentRequest, setSentRequest] = useState(null);
+  const [accepted, setAccepted] = useState(null);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
@@ -21,6 +23,23 @@ const User = ({ profile, sent }) => {
 
     if (result && result.responseData) {
       setSentRequest(true);
+      setLoading(false);
+    }
+  };
+
+  const onAcceptRequest = async () => {
+    setLoading(true);
+
+    let addFriendPayload = { user_id: profile.user_id };
+
+    const result = await FriendFetch(addFriendPayload);
+
+    if (result && result.error) {
+      setError(true);
+    }
+
+    if (result && result.responseData) {
+      setAccepted(true);
       setLoading(false);
     }
   };
@@ -42,16 +61,21 @@ const User = ({ profile, sent }) => {
   }
 
   return (
-    <div className={styles.User}>
-      <div className={styles.avatarContainer}>
-        <img src={avatar}></img>
-      </div>
-      {profile.full_name}
-      {!sent && !sentRequest && (
-        <button onClick={onSendRequest}>Add Friend</button>
+    <>
+      {accepted ? null : (
+        <div className={styles.User}>
+          <div className={styles.avatarContainer}>
+            <img src={avatar}></img>
+          </div>
+          {profile.full_name}
+          {!sent && !received && !sentRequest && (
+            <button onClick={onSendRequest}>Send Request</button>
+          )}
+          {(sent || sentRequest) && <div>Request sent</div>}
+          {received && <button onClick={onAcceptRequest}>Accept Friend</button>}
+        </div>
       )}
-      {(sent || sentRequest) && <div>Request sent</div>}
-    </div>
+    </>
   );
 };
 
