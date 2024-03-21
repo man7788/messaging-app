e25e85b51ae2c2a3f46de118e0ca76b00550ec15
man7788 <jerman1841@gmail.com>
2024-03-22 03:57:57 +0800

    Add logout route

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const { verifyToken } = require("../controllers/verifyToken");
 
 const indexController = require("../controllers/indexController");
 
@@ -13,5 +14,8 @@ router.post("/signup", indexController.sign_up);
 
 // Post request for user log-in
 router.post("/login", indexController.log_in);
+
+// Get request for user log-out
+router.get("/logout", verifyToken, indexController.log_out);
 
 module.exports = router;
