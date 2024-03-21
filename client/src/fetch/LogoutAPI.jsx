cc38d7707fef191609196e6e11b256042e4f780d
man7788 <jerman1841@gmail.com>
2024-03-22 05:55:03 +0800

    Add LogoutAPI

---
+++ client/src/fetch/LogoutAPI.jsx
@@ -0,0 +1,27 @@
+const LogoutFetch = async (sendPayload) => {
+  const token = JSON.parse(localStorage.getItem('token'));
+  try {
+    const response = await fetch(`http://localhost:3000/logout`, {
+      mode: 'cors',
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(sendPayload),
+    });
+
+    if (response.status >= 400) {
+      throw new Error('server error');
+    }
+
+    const responseData = await response.json();
+    console.log(responseData);
+    return { responseData };
+  } catch (error) {
+    console.error(error);
+    return { error: error.message };
+  }
+};
+
+export default LogoutFetch;
