4f03d216bae2cd377f97b96d6ac29c53feee1dc6
man7788 <jerman1841@gmail.com>
2024-03-23 07:25:29 +0800

    Add friend to app

--- api/app.js
+++ api/app.js
@@ -10,6 +10,7 @@ const cors = require("cors");
 const indexRouter = require("./routes/index");
 const userRouter = require("./routes/user");
 const chatRouter = require("./routes/chat");
+const friendRouter = require("./routes/friend");
 
 const app = express();
 app.use(cors());
@@ -37,6 +38,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/user", userRouter);
 app.use("/chat", chatRouter);
+app.use("/friend", friendRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
