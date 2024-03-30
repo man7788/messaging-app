a8b2ee8f6e33d4f059d26cbac84ac3e8a91a5201
man7788 <jerman1841@gmail.com>
2024-03-31 03:31:25 +0800

    Update user router test to use memory server

--- api/routes/user.test.js
+++ api/routes/user.test.js
@@ -1,202 +1,304 @@
+const { connectDB, dropDB, dropCollections } = require("../setuptestdb");
 const request = require("supertest");
 const express = require("express");
-const app = express();
-
 const user = require("./user");
-const User = require("../models/userModel");
-const Profile = require("../models/profileModel");
-const mongoose = require("mongoose");
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", user);
 
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
+const User = require("../models/userModel");
+const Profile = require("../models/profileModel");
+
+beforeAll(async () => {
+  await connectDB();
+});
+
+afterAll(async () => {
+  await dropDB();
+});
+
 afterEach(async () => {
+  await dropCollections();
   jest.clearAllMocks();
 });
+
+jest.mock("bcryptjs");
+jest.mock("jsonwebtoken");
 
 jest.mock("../controllers/verifyToken", () => ({
   verifyToken: jest.fn((req, res, next) => next()),
 }));
 
-jest.mock("jsonwebtoken", () => ({
-  verify: jest.fn((token, secretOrPublicKey, callback) => {
-    return callback(null, { user: { _id: "123" } });
-  }),
-}));
-
-jest.mock("bcryptjs", () => ({
-  compare: jest.fn(() => true),
-  hashSync: jest.fn(() => "hashednewpassword"),
-}));
-
-const userFindSpy = jest.spyOn(User, "find");
-const userFindOneSpy = jest.spyOn(User, "findOne");
-const userFindByIdSpy = jest.spyOn(User, "findById");
-const userFindByIdAndUpdateSpy = jest.spyOn(User, "findByIdAndUpdate");
-
-const profileFindByIdSpy = jest.spyOn(Profile, "findById");
-const profileFindByIdAndUpdateSpy = jest.spyOn(Profile, "findByIdAndUpdate");
-
 describe("user routes", () => {
-  test("responses with a list of all user profiles", async () => {
-    userFindSpy.mockReturnValue({
-      populate: jest.fn().mockResolvedValue([
+  describe("user_status controller", () => {
+    test("responses with verified login user profile", async () => {
+      const userId = new mongoose.Types.ObjectId();
+
+      jwt.verify.mockImplementationOnce(
+        (token, secretOrPublicKey, callback) => {
+          return callback(null, { user: { _id: userId } });
+        }
+      );
+
+      const profile = new Profile({
+        full_name: "john doe",
+      });
+
+      await profile.save();
+
+      const user = new User({
+        email: "john@doe.com",
+        password: "johndoefoobar",
+        profile: profile._id,
+        online: new mongoose.Types.ObjectId(),
+        _id: userId,
+      });
+      await user.save();
+
+      const response = await request(app)
+        .get("/status")
+        .set("Content-Type", "application/json");
+
+      expect(response.header["content-type"]).toMatch(/application\/json/);
+      expect(response.status).toEqual(200);
+
+      expect(response.body.user._id).toMatch(userId.toString());
+      expect(response.body.user.profile._id).toMatch(profile._id.toString());
+      expect(response.body.user.profile.full_name).toMatch("john doe");
+    });
+  });
+
+  describe("profiles controller", () => {
+    test("responses with all user profiles", async () => {
+      const userId = new mongoose.Types.ObjectId();
+
+      jwt.verify.mockImplementationOnce(
+        (token, secretOrPublicKey, callback) => {
+          return callback(null, { user: { _id: userId } });
+        }
+      );
+
+      const profile = new Profile({
+        full_name: "john doe",
+        _id: new mongoose.Types.ObjectId(),
+        about: "foobar",
+      });
+
+      await profile.save();
+
+      await User.insertMany([
         {
           email: "john@doe.com",
-          profile: { _id: "123abc$", full_name: "john doe" },
-          password: "johndoe123",
-          _id: "$abc123",
+          password: "johndoefoobar",
+          profile: profile._id,
+          online: new mongoose.Types.ObjectId(),
         },
         {
-          email: "foo@bar.com",
-          profile: { _id: "$abc123", full_name: "foo bar" },
-          password: "foobar123",
-          _id: "$abc456",
+          email: "john@doe2.com",
+          password: "johndoefoobar2",
+          profile: profile._id,
+          online: new mongoose.Types.ObjectId(),
         },
-      ]),
+        {
+          email: "john@doe3.com",
+          password: "johndoefoobar3",
+          profile: profile._id,
+          online: new mongoose.Types.ObjectId(),
+        },
+      ]);
+
+      const response = await request(app)
+        .get("/profiles")
+        .set("Content-Type", "application/json");
+
+      expect(response.header["content-type"]).toMatch(/application\/json/);
+      expect(response.status).toEqual(200);
+
+      expect(response.body.profiles).toHaveLength(3);
+      for (const i in response.body.profiles) {
+        expect(response.body.profiles[i]).toMatchObject({
+          user_id: expect.anything(),
+          _id: expect.anything(),
+          full_name: expect.anything(),
+          about: expect.anything(),
+        });
+      }
     });
-
-    const resObj = {
-      profiles: [
-        { user_id: "$abc123", _id: "123abc$", full_name: "john doe" },
-        { user_id: "$abc456", _id: "$abc123", full_name: "foo bar" },
-      ],
-    };
-
-    const response = await request(app)
-      .get("/profiles")
-      .set("Content-Type", "application/json");
-
-    expect(response.header["content-type"]).toMatch(/application\/json/);
-    expect(response.status).toEqual(200);
-
-    expect(response.body).toMatchObject(resObj);
   });
 
-  test("responses with verified login user profile", async () => {
-    userFindByIdSpy.mockReturnValue({
-      populate: jest.fn().mockResolvedValueOnce({
-        email: "john@doe.com",
-        profile: { _id: "123abc$", full_name: "john doe" },
-        password: "johndoe123",
-        _id: "$abc123",
-      }),
-    });
+  describe("edit_profile controller", () => {
+    test("responses with updated user with new password", async () => {
+      jwt.verify.mockImplementationOnce(
+        (token, secretOrPublicKey, callback) => {
+          return callback(null, { user: { _id: "some id" } });
+        }
+      );
 
-    const resObj = {
-      user: {
-        email: "john@doe.com",
-        profile: { _id: "123abc$", full_name: "john doe" },
-        password: "johndoe123",
-        _id: "$abc123",
-      },
-    };
+      const profileId = new mongoose.Types.ObjectId();
 
-    const response = await request(app)
-      .get("/status")
-      .set("Content-Type", "application/json");
-
-    expect(response.header["content-type"]).toMatch(/application\/json/);
-    expect(response.status).toEqual(200);
-
-    expect(response.body).toMatchObject(resObj);
-  });
-
-  test("responses with edited profile", async () => {
-    const profile_id = new mongoose.Types.ObjectId();
-
-    userFindOneSpy.mockResolvedValueOnce(null);
-
-    profileFindByIdSpy.mockResolvedValueOnce({
-      _id: profile_id,
-      full_name: "john doe",
-    });
-
-    profileFindByIdAndUpdateSpy.mockResolvedValueOnce({
-      _id: profile_id,
-      full_name: "john doe",
-    });
-
-    const payload = {
-      profile_id: profile_id,
-      new_full_name: "new john doe",
-      new_about: "new about",
-    };
-
-    const resObj = {
-      updated_profile: {
-        _id: profile_id.toString(),
-        full_name: "new john doe",
-        about: "new about",
-      },
-
-      previous_profile: {
-        _id: profile_id.toString(),
+      const profile = new Profile({
         full_name: "john doe",
-      },
-    };
+        about: "my name is john doe",
+        _id: profileId,
+      });
+      profile.save();
 
-    const response = await request(app)
-      .post("/profile/edit")
-      .set("Content-Type", "application/json")
-      .send(payload);
+      const payload = {
+        profile_id: profileId,
+        new_full_name: "new full name",
+        new_about: "new about",
+      };
 
-    expect(response.header["content-type"]).toMatch(/application\/json/);
-    expect(response.status).toEqual(200);
+      const response = await request(app)
+        .post("/profile/edit")
+        .set("Content-Type", "application/json")
+        .send(payload);
 
-    expect(response.body).toMatchObject(resObj);
+      expect(response.header["content-type"]).toMatch(/application\/json/);
+      expect(response.status).toEqual(200);
+
+      expect(response.body.updated_profile).toMatchObject({
+        full_name: "new full name",
+        about: "new about",
+        _id: profileId.toString(),
+      });
+    });
   });
 
-  test("responses with edited user", async () => {
-    const profile_id = new mongoose.Types.ObjectId();
-    const user_id = new mongoose.Types.ObjectId();
+  describe("edit_password controller", () => {
+    test("responses with incorrect current password error", async () => {
+      jwt.verify.mockImplementationOnce(
+        (token, secretOrPublicKey, callback) => {
+          return callback(null, { user: { _id: "some id" } });
+        }
+      );
 
-    userFindByIdSpy.mockResolvedValue({
-      email: "john@doe.com",
-      profile: profile_id,
-      password: "johndoe123",
-      _id: user_id,
+      bcrypt.compare.mockImplementationOnce(() => false);
+
+      const userId = new mongoose.Types.ObjectId();
+      const profileId = new mongoose.Types.ObjectId();
+      const onlineId = new mongoose.Types.ObjectId();
+
+      const user = new User({
+        email: "john@doe.com",
+        password: "johndoefoobar",
+        profile: profileId,
+        online: onlineId,
+        _id: userId,
+      });
+      await user.save();
+
+      const payload = {
+        user_id: userId,
+        current_password: "wrongpassword",
+        new_password: "newpassword",
+        confirm_new_password: "newpassword",
+      };
+
+      const response = await request(app)
+        .post("/password/edit")
+        .set("Content-Type", "application/json")
+        .send(payload);
+
+      expect(response.header["content-type"]).toMatch(/application\/json/);
+      expect(response.status).toEqual(200);
+
+      expect(response.body.errors[0].msg).toMatch("Incorrect current password");
     });
 
-    userFindByIdAndUpdateSpy.mockResolvedValueOnce({
-      email: "john@doe.com",
-      profile: profile_id,
-      password: "johndoe123",
-      _id: user_id,
+    test("responses with new passwords do not match error", async () => {
+      jwt.verify.mockImplementationOnce(
+        (token, secretOrPublicKey, callback) => {
+          return callback(null, { user: { _id: "some id" } });
+        }
+      );
+
+      bcrypt.compare.mockImplementationOnce(() => true);
+
+      const userId = new mongoose.Types.ObjectId();
+      const profileId = new mongoose.Types.ObjectId();
+      const onlineId = new mongoose.Types.ObjectId();
+
+      const user = new User({
+        email: "john@doe.com",
+        password: "johndoefoobar",
+        profile: profileId,
+        online: onlineId,
+        _id: userId,
+      });
+      await user.save();
+
+      const payload = {
+        user_id: userId,
+        current_password: "johndoefoobar",
+        new_password: "newpassword",
+        confirm_new_password: "newpasswordnotmatch",
+      };
+
+      const response = await request(app)
+        .post("/password/edit")
+        .set("Content-Type", "application/json")
+        .send(payload);
+
+      expect(response.header["content-type"]).toMatch(/application\/json/);
+      expect(response.status).toEqual(200);
+
+      expect(response.body.errors[0].msg).toMatch("New passwords do not match");
     });
 
-    const payload = {
-      user_id: user_id,
-      current_password: "password",
-      new_password: "newpassword",
-      confirm_new_password: "newpassword",
-    };
+    test("responses with updated user with new password", async () => {
+      jwt.verify.mockImplementationOnce(
+        (token, secretOrPublicKey, callback) => {
+          return callback(null, { user: { _id: "some id" } });
+        }
+      );
 
-    const resObj = {
-      updated_user: {
+      bcrypt.compare.mockImplementationOnce(() => true);
+
+      bcrypt.hashSync.mockImplementationOnce((password, salt) => {
+        return "hashedpassword";
+      });
+
+      const userId = new mongoose.Types.ObjectId();
+      const profileId = new mongoose.Types.ObjectId();
+      const onlineId = new mongoose.Types.ObjectId();
+
+      const previosUser = new User({
         email: "john@doe.com",
-        password: "hashednewpassword",
-        profile: profile_id.toString(),
-        _id: user_id.toString(),
-      },
+        password: "johndoefoobar",
+        profile: profileId,
+        online: onlineId,
+        _id: userId,
+      });
+      await previosUser.save();
 
-      previous_user: {
+      const payload = {
+        user_id: userId,
+        current_password: "johndoefoobar",
+        new_password: "newpassword",
+        confirm_new_password: "newpassword",
+      };
+
+      const response = await request(app)
+        .post("/password/edit")
+        .set("Content-Type", "application/json")
+        .send(payload);
+
+      expect(response.header["content-type"]).toMatch(/application\/json/);
+      expect(response.status).toEqual(200);
+
+      expect(response.body.updated_user).toMatchObject({
         email: "john@doe.com",
-        password: "johndoe123",
-        profile: profile_id.toString(),
-        _id: user_id.toString(),
-      },
-    };
-
-    const response = await request(app)
-      .post("/password/edit")
-      .set("Content-Type", "application/json")
-      .send(payload);
-
-    expect(response.header["content-type"]).toMatch(/application\/json/);
-    expect(response.status).toEqual(200);
-
-    expect(response.body).toMatchObject(resObj);
+        password: "hashedpassword",
+        profile: profileId.toString(),
+        online: onlineId.toString(),
+        _id: userId.toString(),
+      });
+    });
   });
 });
