62a027e214b8b893f89e30c3585c31f1adcb2fc7
man7788 <jerman1841@gmail.com>
2024-03-31 03:31:16 +0800

    Update index router test to use memory server

--- api/routes/index.test.js
+++ api/routes/index.test.js
@@ -1,3 +1,4 @@
+const { connectDB, dropDB, dropCollections } = require("../setuptestdb");
 const request = require("supertest");
 const express = require("express");
 const index = require("./index");
@@ -7,38 +8,76 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/", index);
 
-const mongoose = require("mongoose");
 const bcrypt = require("bcryptjs");
+const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
-
 const User = require("../models/userModel");
-const Profile = require("../models/profileModel");
 const Online = require("../models/onlineModel");
 
-const userFindOneSpy = jest.spyOn(User, "findOne");
-const userFindByIdUpdateSpy = jest.spyOn(User, "findByIdAndUpdate");
-const userSaveSpy = jest.spyOn(User.prototype, "save");
-const profileSaveSpy = jest.spyOn(Profile.prototype, "save");
-const onlineSaveSpy = jest.spyOn(Online.prototype, "save");
-const onlineFindOneSpy = jest.spyOn(Online, "findOne");
-const onlineFindByIdUpdateSpy = jest.spyOn(Online, "findByIdAndUpdate");
+beforeAll(async () => {
+  await connectDB();
+});
 
-afterEach(() => {
+afterAll(async () => {
+  await dropDB();
+});
+
+afterEach(async () => {
+  await dropCollections();
   jest.clearAllMocks();
-  jest.resetAllMocks();
 });
 
 jest.mock("bcryptjs");
 jest.mock("jsonwebtoken");
 
-const profile_id = new mongoose.Types.ObjectId();
-const user_id = new mongoose.Types.ObjectId();
-const online_id = new mongoose.Types.ObjectId();
+jest.mock("../controllers/verifyToken", () => ({
+  verifyToken: jest.fn((req, res, next) => next()),
+}));
 
 describe("index routes", () => {
   describe("sign-up controller", () => {
-    beforeEach(() => {
-      userFindOneSpy.mockResolvedValueOnce(null);
+    test("responses with email already in use error", async () => {
+      const user = new User({
+        email: "john@doe.com",
+        password: "johndoefoobar",
+        profile: new mongoose.Types.ObjectId(),
+        online: new mongoose.Types.ObjectId(),
+      });
+      await user.save();
+
+      const payload = {
+        email: "john@doe.com",
+        full_name: "john doe",
+        password: "johndoefoobar",
+        confirm_password: "johndoefoobar",
+      };
+
+      const response = await request(app)
+        .post("/signup")
+        .set("Content-Type", "application/json")
+        .send(payload);
+
+      expect(response.header["content-type"]).toMatch(/application\/json/);
+      expect(response.status).toEqual(200);
+      expect(response.body.errors[0].msg).toBe("Email already in use");
+    });
+
+    test("responses with passwords do not match error", async () => {
+      const payload = {
+        email: "john@doe.com",
+        full_name: "john doe",
+        password: "johndoefoobar",
+        confirm_password: "notmatchingpassword",
+      };
+
+      const response = await request(app)
+        .post("/signup")
+        .set("Content-Type", "application/json")
+        .send(payload);
+
+      expect(response.header["content-type"]).toMatch(/application\/json/);
+      expect(response.status).toEqual(200);
+      expect(response.body.errors[0].msg).toBe("Passwords do not match");
     });
 
     test("successful sign-up responses with created user", async () => {
@@ -46,36 +85,11 @@ describe("index routes", () => {
         return callback(null, "hashedpassword");
       });
 
-      profileSaveSpy.mockResolvedValueOnce({
-        full_name: "foobar",
-        _id: profile_id,
-      });
-
-      userSaveSpy.mockResolvedValueOnce({
-        email: "john@doe.com",
-        full_name: "foobar",
-        password: "hashedpassword",
-        profile: profile_id,
-        online: online_id,
-        _id: user_id,
-      });
-
-      onlineSaveSpy.mockResolvedValueOnce({
-        online: false,
-      });
-
       const payload = {
         email: "john@doe.com",
-        full_name: "foobar",
+        full_name: "john doe",
         password: "johndoefoobar",
         confirm_password: "johndoefoobar",
-      };
-
-      const resObj = {
-        email: "john@doe.com",
-        password: "hashedpassword",
-        profile: profile_id.toString(),
-        _id: user_id.toString(),
       };
 
       const response = await request(app)
@@ -86,97 +100,112 @@ describe("index routes", () => {
       expect(response.header["content-type"]).toMatch(/application\/json/);
       expect(response.status).toEqual(200);
 
-      expect(response.body).toMatchObject(resObj);
+      expect(response.body.email).toMatch("john@doe.com");
+      expect(response.body.password).toMatch("hashedpassword");
+      expect(response.body.profile.full_name).toMatch("john doe");
+      expect(response.body.online.online).toBe(false);
     });
   });
 
   describe("log-in controller", () => {
+    describe("failed log-in", () => {
+      test("responses with user not found error ", async () => {
+        const payload = {
+          email: "john@doe.com",
+          password: "johndoefoobar",
+        };
+
+        const response = await request(app)
+          .post("/login")
+          .set("Content-Type", "application/json")
+          .send(payload);
+
+        expect(response.header["content-type"]).toMatch(/application\/json/);
+        expect(response.status).toEqual(200);
+
+        expect(response.body.errors[0].msg).toMatch(/user not found/i);
+      });
+
+      test("manual log-in responses with incorrect password error ", async () => {
+        bcrypt.compare.mockImplementationOnce(() => false);
+
+        const user = new User({
+          email: "john@doe.com",
+          password: "johndoefoobar",
+          profile: new mongoose.Types.ObjectId(),
+          online: new mongoose.Types.ObjectId(),
+        });
+        await user.save();
+
+        const payload = {
+          email: "john@doe.com",
+          password: "johndoefoobar",
+        };
+
+        const response = await request(app)
+          .post("/login")
+          .set("Content-Type", "application/json")
+          .send(payload);
+
+        expect(response.header["content-type"]).toMatch(/application\/json/);
+        expect(response.status).toEqual(200);
+
+        expect(response.body.errors[0].msg).toMatch(/incorrect password/i);
+      });
+
+      test("auto log-in responses with incorrect password error ", async () => {
+        const user = new User({
+          email: "john@doe.com",
+          password: "johndoefoobar",
+          profile: new mongoose.Types.ObjectId(),
+          online: new mongoose.Types.ObjectId(),
+        });
+        await user.save();
+
+        const payload = {
+          email: "john@doe.com",
+          password: "johndoefoobar",
+          auto_login: "wrong password",
+        };
+
+        const response = await request(app)
+          .post("/login")
+          .set("Content-Type", "application/json")
+          .send(payload);
+
+        expect(response.header["content-type"]).toMatch(/application\/json/);
+        expect(response.status).toEqual(200);
+
+        expect(response.body.errors[0].msg).toMatch(/incorrect password/i);
+      });
+    });
+
     describe("successful log-in", () => {
-      test("manual log-in responses with token if user has no online property", async () => {
-        userFindOneSpy.mockResolvedValueOnce({ toObject: jest.fn() });
-        bcrypt.compare.mockImplementationOnce(() => true);
+      test("auto log-in responses with token if user has no online property", async () => {
         jwt.sign.mockImplementationOnce(
           (token, secretOrPublicKey, options, callback) => {
             return callback(null, "123abc$");
           }
         );
-        onlineSaveSpy.mockResolvedValueOnce({});
-        userFindByIdUpdateSpy.mockResolvedValueOnce({});
 
-        const payload = {
+        const user = new User({
           email: "john@doe.com",
           password: "johndoefoobar",
-        };
+          profile: new mongoose.Types.ObjectId(),
+        });
+        await user.save({ validateBeforeSave: false });
 
-        const response = await request(app)
-          .post("/login")
-          .set("Content-Type", "application/json")
-          .send(payload);
+        const offlineUser = await User.findOne({
+          email: "john@doe.com",
+        }).populate("online");
 
-        expect(response.header["content-type"]).toMatch(/application\/json/);
-        expect(response.status).toEqual(200);
-
-        expect(response.body.token).toBe("123abc$");
-      });
-
-      test("manual log-in responses with token", async () => {
-        userFindOneSpy.mockResolvedValueOnce({ online: online_id });
-        bcrypt.compare.mockImplementationOnce(() => true);
-        jwt.sign.mockImplementationOnce(
-          (token, secretOrPublicKey, options, callback) => {
-            return callback(null, "123abc$");
-          }
+        expect(offlineUser).toEqual(
+          expect.not.objectContaining({ online: false })
         );
-        onlineFindOneSpy.mockResolvedValueOnce({
-          online: false,
-          _id: online_id,
-        });
-
-        onlineFindByIdUpdateSpy.mockResolvedValueOnce({
-          online: false,
-          _id: online_id,
-        });
 
         const payload = {
           email: "john@doe.com",
           password: "johndoefoobar",
-        };
-
-        const response = await request(app)
-          .post("/login")
-          .set("Content-Type", "application/json")
-          .send(payload);
-
-        expect(response.header["content-type"]).toMatch(/application\/json/);
-        expect(response.status).toEqual(200);
-
-        expect(response.body.token).toBe("123abc$");
-      });
-
-      test("auto log-in responses with token", async () => {
-        userFindOneSpy.mockResolvedValueOnce({
-          email: "john@doe.com",
-          password: "johndoefoobar",
-          online: online_id,
-        });
-        jwt.sign.mockImplementationOnce(
-          (token, secretOrPublicKey, options, callback) => {
-            return callback(null, "123abc$");
-          }
-        );
-        onlineFindOneSpy.mockResolvedValueOnce({
-          online: false,
-          _id: online_id,
-        });
-
-        onlineFindByIdUpdateSpy.mockResolvedValueOnce({
-          online: false,
-          _id: online_id,
-        });
-
-        const payload = {
-          email: "john@doe.com",
-          password: "placeholder",
           auto_login: "johndoefoobar",
         };
 
@@ -185,74 +214,192 @@ describe("index routes", () => {
           .set("Content-Type", "application/json")
           .send(payload);
 
+        const onlineUser = await User.findOne({
+          email: "john@doe.com",
+        }).populate("online");
+
         expect(response.header["content-type"]).toMatch(/application\/json/);
         expect(response.status).toEqual(200);
 
-        expect(response.body.token).toBe("123abc$");
+        expect(onlineUser.online.online).toBe(true);
+        expect(response.body.token).toMatch("123abc$");
+      });
+
+      test("auto log-in responses with token if user has online property", async () => {
+        jwt.sign.mockImplementationOnce(
+          (token, secretOrPublicKey, options, callback) => {
+            return callback(null, "123abc$");
+          }
+        );
+
+        const online = new Online({ online: false });
+        await online.save();
+
+        const user = new User({
+          email: "john@doe.com",
+          password: "johndoefoobar",
+          profile: new mongoose.Types.ObjectId(),
+          online: online._id,
+        });
+        await user.save();
+
+        const offlineUser = await User.findOne({
+          email: "john@doe.com",
+        }).populate("online");
+
+        expect(offlineUser.online.online).toBe(false);
+
+        const payload = {
+          email: "john@doe.com",
+          password: "johndoefoobar",
+          auto_login: "johndoefoobar",
+        };
+
+        const response = await request(app)
+          .post("/login")
+          .set("Content-Type", "application/json")
+          .send(payload);
+
+        const onlineUser = await User.findOne({
+          email: "john@doe.com",
+        }).populate("online");
+
+        expect(response.header["content-type"]).toMatch(/application\/json/);
+        expect(response.status).toEqual(200);
+
+        expect(onlineUser.online.online).toBe(true);
+        expect(response.body.token).toMatch("123abc$");
+      });
+
+      test("manual log-in responses with token if user has no online property", async () => {
+        bcrypt.compare.mockImplementationOnce(() => true);
+        jwt.sign.mockImplementationOnce(
+          (token, secretOrPublicKey, options, callback) => {
+            return callback(null, "123abc$");
+          }
+        );
+
+        const user = new User({
+          email: "john@doe.com",
+          password: "johndoefoobar",
+          profile: new mongoose.Types.ObjectId(),
+        });
+        await user.save({ validateBeforeSave: false });
+
+        const offlineUser = await User.findOne({
+          email: "john@doe.com",
+        }).populate("online");
+
+        expect(offlineUser).toEqual(
+          expect.not.objectContaining({ online: false })
+        );
+
+        const payload = {
+          email: "john@doe.com",
+          password: "johndoefoobar",
+        };
+
+        const response = await request(app)
+          .post("/login")
+          .set("Content-Type", "application/json")
+          .send(payload);
+
+        const onlineUser = await User.findOne({
+          email: "john@doe.com",
+        }).populate("online");
+
+        expect(response.header["content-type"]).toMatch(/application\/json/);
+        expect(response.status).toEqual(200);
+
+        expect(onlineUser.online.online).toBe(true);
+        expect(response.body.token).toMatch("123abc$");
+      });
+
+      test("manual log-in responses with token if user has online property", async () => {
+        bcrypt.compare.mockImplementationOnce(() => true);
+
+        jwt.sign.mockImplementationOnce(
+          (token, secretOrPublicKey, options, callback) => {
+            return callback(null, "123abc$");
+          }
+        );
+
+        const online = new Online({ online: false });
+        await online.save();
+
+        const user = new User({
+          email: "john@doe.com",
+          password: "johndoefoobar",
+          profile: new mongoose.Types.ObjectId(),
+          online: online._id,
+        });
+        await user.save();
+
+        const offlineUser = await User.findOne({
+          email: "john@doe.com",
+        }).populate("online");
+
+        expect(offlineUser.online.online).toBe(false);
+
+        const payload = {
+          email: "john@doe.com",
+          password: "johndoefoobar",
+        };
+
+        const response = await request(app)
+          .post("/login")
+          .set("Content-Type", "application/json")
+          .send(payload);
+
+        const onlineUser = await User.findOne({
+          email: "john@doe.com",
+        }).populate("online");
+
+        expect(response.header["content-type"]).toMatch(/application\/json/);
+        expect(response.status).toEqual(200);
+
+        expect(onlineUser.online.online).toBe(true);
+        expect(response.body.token).toMatch("123abc$");
       });
     });
+  });
 
-    describe("failed log-in", () => {
-      test("responses with user not found", async () => {
-        userFindOneSpy.mockResolvedValueOnce(null);
+  describe("logout controller", () => {
+    test("log-out responses with upadted online ", async () => {
+      const userId = new mongoose.Types.ObjectId();
 
-        const payload = {
-          email: "john@doe.com",
-          password: "johndoefoobar",
-        };
+      jwt.verify.mockImplementationOnce(
+        (token, secretOrPublicKey, callback) => {
+          return callback(null, { user: { _id: userId } });
+        }
+      );
 
-        const response = await request(app)
-          .post("/login")
-          .set("Content-Type", "application/json")
-          .send(payload);
+      const online = new Online({ online: true });
+      await online.save();
 
-        expect(response.header["content-type"]).toMatch(/application\/json/);
-        expect(response.status).toEqual(200);
-
-        expect(response.body.errors[0].msg).toMatch(/User Not Found/i);
+      const user = new User({
+        email: "john@doe.com",
+        password: "johndoefoobar",
+        profile: new mongoose.Types.ObjectId(),
+        online: online._id,
+        _id: userId,
       });
+      await user.save();
 
-      test("manual log-in responses with incorrect password", async () => {
-        userFindOneSpy.mockResolvedValueOnce(true);
-        bcrypt.compare.mockImplementationOnce(() => false);
+      const onlineUser = await User.findOne({
+        email: "john@doe.com",
+      }).populate("online");
 
-        const payload = {
-          email: "john@doe.com",
-          password: "johndoefoobar",
-        };
+      expect(onlineUser.online.online).toBe(true);
 
-        const response = await request(app)
-          .post("/login")
-          .set("Content-Type", "application/json")
-          .send(payload);
+      const response = await request(app)
+        .get("/logout")
+        .set("Content-Type", "application/json");
 
-        expect(response.header["content-type"]).toMatch(/application\/json/);
-        expect(response.status).toEqual(200);
+      expect(response.header["content-type"]).toMatch(/application\/json/);
+      expect(response.status).toEqual(200);
 
-        expect(response.body.errors[0].msg).toMatch(/Incorrect Password/i);
-      });
-
-      test("auto log-in responses with incorrect password", async () => {
-        userFindOneSpy.mockResolvedValueOnce({
-          email: "john@doe.com",
-          password: "johndoefoobar",
-        });
-
-        const payload = {
-          email: "john@doe.com",
-          password: "wrongpassword",
-        };
-
-        const response = await request(app)
-          .post("/login")
-          .set("Content-Type", "application/json")
-          .send(payload);
-
-        expect(response.header["content-type"]).toMatch(/application\/json/);
-        expect(response.status).toEqual(200);
-
-        expect(response.body.errors[0].msg).toMatch(/Incorrect Password/i);
-      });
+      expect(response.body.updatedOnline.online.online).toBe(false);
     });
   });
 });
