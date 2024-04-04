1409f9d2606dfcedfdb49e34b08bd2ba391f72d6
man7788 <jerman1841@gmail.com>
2024-04-04 09:39:34 +0800

    Add send group message controller

--- api/controllers/groupController.js
+++ api/controllers/groupController.js
@@ -53,3 +53,49 @@ exports.group_chat = asyncHandler(async (req, res, next) => {
     }
   });
 });
+
+// Handle send group message on POST
+exports.group_message = [
+  body("message")
+    .trim()
+    .isLength({ min: 1, max: 10000 })
+    .withMessage("Message must not be empty"),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+
+    if (!errors.isEmpty()) {
+      res.json({
+        errors: errors.array(),
+      });
+    } else {
+      jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
+        if (err) {
+          res.json({ error: "invalid token" });
+        } else {
+          const userList = [authData.user._id, ...req.body.user_id_list];
+
+          const group = await Group.findOne({
+            users: userList,
+          });
+
+          if (group) {
+            const message = new Message({
+              chat: group._id,
+              text: req.body.message,
+              author: authData.user._id,
+              date: new Date(),
+              chatModel: "Group",
+            });
+
+            const createdMessage = await message.save();
+
+            res.json({ createdMessage });
+          } else {
+            res.json(null);
+          }
+        }
+      });
+    }
+  }),
+];
