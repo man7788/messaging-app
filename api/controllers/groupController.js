389f0c08f5fb7f71809d821b5658075a1b9e6cd1
man7788 <jerman1841@gmail.com>
2024-04-04 08:18:28 +0800

    Add request group chat controller

--- api/controllers/groupController.js
+++ api/controllers/groupController.js
@@ -1,8 +1,7 @@
 const { body, validationResult } = require("express-validator");
 const asyncHandler = require("express-async-handler");
 const jwt = require("jsonwebtoken");
-// const Chat = require("../models/chatModel");
-// const Message = require("../models/messageModel");
+const Message = require("../models/messageModel");
 const Group = require("../models/groupModel");
 
 // Handle request group create on POST
@@ -25,6 +24,31 @@ exports.create_group = asyncHandler(async (req, res, next) => {
         await group.save();
 
         res.json({ group });
+      }
+    }
+  });
+});
+
+// Handle request for group chat on POST
+exports.group_chat = asyncHandler(async (req, res, next) => {
+  jwt.verify(req.token, process.env.JWT_SECRET, async (err, authData) => {
+    if (err) {
+      res.json({ error: "invalid token" });
+    } else {
+      const userList = [authData.user._id, ...req.body.user_id_list];
+
+      const group = await Group.findOne({
+        users: { $all: [...userList] },
+      });
+
+      if (group) {
+        const messages = await Message.find({ chat: group._id }).sort({
+          date: 1,
+        });
+
+        res.json({ messages });
+      } else {
+        res.json({ messages: null });
       }
     }
   });
