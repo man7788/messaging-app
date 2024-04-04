21e595c47600c7723fd9543feee783cafe78f61b
man7788 <jerman1841@gmail.com>
2024-04-04 10:09:13 +0800

    Add send group image route

--- api/routes/group.js
+++ api/routes/group.js
@@ -11,7 +11,15 @@ router.post("/create", verifyToken, groupController.create_group);
 // Post request for group chat
 router.post("/chat", verifyToken, groupController.group_chat);
 
-// Post request for group chat
+// Post request for send group message
 router.post("/send", verifyToken, groupController.group_message);
+
+// Post request for send group image
+router.post(
+  "/send/image",
+  verifyToken,
+  upload.single("msgImage"),
+  groupController.group_image
+);
 
 module.exports = router;
