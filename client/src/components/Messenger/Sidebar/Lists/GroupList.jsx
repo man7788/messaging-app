f964de837e0a1a427acf3189d59b3b8f6c0feed8
man7788 <jerman1841@gmail.com>
2024-04-07 03:39:39 +0800

    Add group create form

--- client/src/components/Messenger/Sidebar/Lists/GroupList.jsx
+++ client/src/components/Messenger/Sidebar/Lists/GroupList.jsx
@@ -1,18 +1,34 @@
 import styles from './GroupList.module.css';
-import Group from './Group';
 import { useEffect, useRef, useState } from 'react';
+import Group from './Group';
+import GroupCreateFetch from '../../../../fetch/groups/GroupCreateAPI';
+import foward from '../../../../images/foward.svg';
 
 const GroupList = ({
   loginId,
   friends,
   friendsLoading,
   friendsError,
-  groupList,
-  setGroupList,
+  onShowFriends,
 }) => {
   const listRef = useRef();
   const [renderList, setRenderList] = useState(null);
   const [isOverFlow, setIsOverFlow] = useState(null);
+  const [groupName, setGroupName] = useState('');
+  const [groupList, setGroupList] = useState([]);
+
+  const [loading, setLoading] = useState(null);
+  const [serverError, setServerError] = useState(null);
+  const [formErrors, setFormErrors] = useState([]);
+
+  useEffect(() => {
+    setGroupName('');
+    setGroupList([]);
+  }, []);
+
+  useEffect(() => {
+    setFormErrors([]);
+  }, [groupName, groupList]);
 
   useEffect(() => {
     if (friends && friends.length > 0) {
@@ -24,7 +40,36 @@ const GroupList = ({
     }
   }, [friends]);
 
-  if (friendsError) {
+  const onSubmitForm = async (e) => {
+    e.preventDefault();
+
+    setLoading(true);
+
+    const groupPayload = {
+      group_name: groupName,
+      user_id_list: groupList,
+    };
+
+    const result = await GroupCreateFetch(groupPayload);
+
+    if (result && result.error) {
+      setServerError(true);
+    }
+
+    if (result && result.formErrors) {
+      setFormErrors(result.formErrors);
+      setLoading(false);
+    }
+
+    if (result && result.responseData) {
+      setLoading(false);
+      setGroupName('');
+      setGroupList([]);
+      onShowFriends();
+    }
+  };
+
+  if (friendsError || serverError) {
     return (
       <div className={styles.error} data-testid="error">
         <h1>A network error was encountered</h1>
@@ -32,7 +77,7 @@ const GroupList = ({
     );
   }
 
-  if (friendsLoading) {
+  if (friendsLoading || loading) {
     return (
       <div className={styles.loading} data-testid="loading">
         <div className={styles.loader}></div>
@@ -45,6 +90,22 @@ const GroupList = ({
       className={isOverFlow ? styles.GroupListFlow : styles.Group}
       ref={listRef}
     >
+      <div className={styles.createForm}>
+        <form action="" method="post" onSubmit={onSubmitForm}>
+          <div className={styles.createError}>{formErrors[0]?.msg}</div>
+          <input
+            type="text"
+            name="out_message"
+            id="out_message"
+            placeholder="Group name"
+            value={groupName}
+            onChange={(event) => setGroupName(event.target.value)}
+          ></input>
+          <button>
+            <img src={foward}></img>
+          </button>
+        </form>
+      </div>
       {renderList ? (
         friends.map((friend) => {
           if (friend.user_id !== loginId) {
