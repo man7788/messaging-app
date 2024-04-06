84cf0795c9cce663e5300f333b47c4c1f09ea4d5
man7788 <jerman1841@gmail.com>
2024-04-07 03:38:08 +0800

    Add group create api

---
+++ client/src/fetch/groups/GroupCreateAPI.jsx
@@ -0,0 +1,31 @@
+const GroupCreateFetch = async (sendPayload) => {
+  const token = JSON.parse(localStorage.getItem('token'));
+  try {
+    const response = await fetch(`http://localhost:3000/group/create`, {
+      mode: 'cors',
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`,
+      },
+      body: JSON.stringify(sendPayload),
+    });
+
+    if (response.status >= 400) {
+      throw new Error('server error');
+    }
+    const responseData = await response.json();
+
+    if (responseData && responseData.errors) {
+      return { formErrors: responseData.errors };
+    }
+
+    console.log(responseData);
+    return { responseData };
+  } catch (error) {
+    console.error(error);
+    return { error: error.message };
+  }
+};
+
+export default GroupCreateFetch;
