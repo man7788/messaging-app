80e62da39a77d9973443c13df06adf9ea4e09f80
man7788 <jerman1841@gmail.com>
2024-04-07 03:26:19 +0800

    Add name property to group model

--- api/models/groupModel.js
+++ api/models/groupModel.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const GroupSchema = new Schema({
+  name: { type: String, minLength: 1, maxLength: 50, require: true },
   users: [{ type: Schema.Types.ObjectId, ref: "User", require: true }],
 });
 
