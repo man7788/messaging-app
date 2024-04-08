77414d04d6dd9d009f7b6a1c1ac29c5a5123f250
man7788 <jerman1841@gmail.com>
2024-04-09 01:56:00 +0800

    Add useGroupsAPI

---
+++ client/src/fetch/groups/useGroupsAPI.jsx
@@ -0,0 +1,52 @@
+import { useEffect, useState } from 'react';
+
+const useGroups = () => {
+  const [groups, setGroups] = useState(null);
+  const [groupsLoading, setGroupsLoading] = useState(true);
+  const [groupsError, setGroupsError] = useState(null);
+  const [updateGroups, setUpdateGroups] = useState(null);
+
+  useEffect(() => {
+    const token = JSON.parse(localStorage.getItem('token'));
+    const fetchGroups = async () => {
+      if (token !== undefined) {
+        try {
+          const response = await fetch(`http://localhost:3000/group/all`, {
+            mode: 'cors',
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/json',
+              Authorization: `Bearer ${token}`,
+            },
+          });
+          if (response.status >= 400) {
+            throw new Error('server error');
+          }
+          const responseData = await response.json();
+
+          if (responseData && responseData.error) {
+            setGroups(responseData);
+          } else if (responseData && responseData.groups) {
+            console.log(responseData.groups);
+            setGroups(responseData.groups);
+          }
+        } catch (error) {
+          setGroupsError(error.message);
+        } finally {
+          setGroupsLoading(false);
+        }
+      }
+    };
+    fetchGroups();
+  }, [updateGroups]);
+
+  return {
+    groups,
+    groupsLoading,
+    groupsError,
+    updateGroups,
+    setUpdateGroups,
+  };
+};
+
+export default useGroups;
