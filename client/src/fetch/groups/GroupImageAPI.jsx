f777658118120014dfbe843e5068d9895d5df93d
man7788 <jerman1841@gmail.com>
2024-04-09 05:13:21 +0800

    Add GroupImageAPI

---
+++ client/src/fetch/groups/GroupImageAPI.jsx
@@ -0,0 +1,35 @@
+const GroupImageFetch = async (sendPayload) => {
+  const newForm = new FormData();
+
+  newForm.append('group_id', sendPayload.group_id);
+  newForm.append('msgImage', sendPayload.image);
+
+  const token = JSON.parse(localStorage.getItem('token'));
+  try {
+    const response = await fetch(`http://localhost:3000/group/send/image`, {
+      mode: 'cors',
+      method: 'POST',
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+      body: newForm,
+    });
+
+    if (response.status >= 400) {
+      throw new Error('server error');
+    }
+
+    const responseData = await response.json();
+
+    if (responseData && responseData.errors) {
+      return { formErrors: responseData.errors };
+    }
+    console.log(responseData);
+    return { responseData };
+  } catch (error) {
+    console.error(error);
+    return { error: error.message };
+  }
+};
+
+export default GroupImageFetch;
