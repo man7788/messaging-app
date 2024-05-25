f1777bde14bbaa5830eb888a38862be190fd63f6
man7788 <jerman1841@gmail.com>
2024-05-25 11:12:38 +0800

    Add form submit error test

--- client/src/components/Messenger/Sidebar/Lists/NewGroupList.test.jsx
+++ client/src/components/Messenger/Sidebar/Lists/NewGroupList.test.jsx
@@ -1,8 +1,12 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, waitFor, act } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { chatContext } from '../../../../contexts/chatContext';
 import NewGroupList from './NewGroupList';
 import * as GroupCreateFetch from '../../../../fetch/groups/GroupCreateAPI';
+
+afterEach(() => {
+  vi.clearAllMocks();
+});
 
 const GroupCreateFetchSpy = vi.spyOn(GroupCreateFetch, 'default');
 
@@ -133,6 +137,50 @@ describe('New group form', () => {
     expect(checkbox.checked).toBe(false);
   });
 
+  test('should show error after form submit', async () => {
+    const user = userEvent.setup();
+
+    GroupCreateFetchSpy.mockReturnValue({
+      error: true,
+    });
+
+    render(
+      <chatContext.Provider value={{ setChatProfile: vi.fn() }}>
+        <NewGroupList
+          friends={[
+            {
+              user_id: '1002',
+              _id: '22',
+              full_name: 'foobar2',
+              online: true,
+            },
+          ]}
+          friendsLoading={false}
+          friendsError={false}
+          onShowFriends={vi.fn()}
+          setChangeSlide={vi.fn()}
+          onShowChats={vi.fn()}
+          setChatId={vi.fn()}
+        />
+        ,
+      </chatContext.Provider>,
+    );
+
+    const input = await screen.findByRole('textbox');
+    const checkbox = await screen.findByRole('checkbox');
+
+    await user.type(input, 'group name');
+    await user.click(checkbox);
+
+    const submit = await screen.findByTestId('new-group-submit');
+
+    await user.click(submit);
+
+    const error = await screen.findByTestId('error');
+
+    expect(error).toBeInTheDocument;
+  });
+
   test('should submit form', async () => {
     const user = userEvent.setup();
     const createdGroup = { name: 'new group', users: ['id1', 'id2'] };
@@ -175,5 +223,50 @@ describe('New group form', () => {
       group_name: 'group name',
       user_id_list: ['1002'],
     });
+  });
+
+  test('should not submit form if group name or group member is empty', async () => {
+    const user = userEvent.setup();
+
+    render(
+      <chatContext.Provider value={{ setChatProfile: vi.fn() }}>
+        <NewGroupList
+          friends={[
+            {
+              user_id: '1002',
+              _id: '22',
+              full_name: 'foobar2',
+              online: true,
+            },
+          ]}
+          friendsLoading={false}
+          friendsError={false}
+          onShowFriends={vi.fn()}
+          setChangeSlide={vi.fn()}
+          onShowChats={vi.fn()}
+          setChatId={vi.fn()}
+        />
+        ,
+      </chatContext.Provider>,
+    );
+
+    const input = await screen.findByRole('textbox');
+    const checkbox = await screen.findByRole('checkbox');
+
+    await waitFor(() =>
+      expect(screen.queryByTestId('new-group-submit')).toBe(null),
+    );
+    await user.type(input, '{enter}');
+
+    expect(GroupCreateFetchSpy).not.toHaveBeenCalled();
+
+    await user.clear(input);
+    await user.click(checkbox);
+    await waitFor(() =>
+      expect(screen.queryByTestId('new-group-submit')).toBe(null),
+    );
+    await user.type(input, '{enter}');
+
+    expect(GroupCreateFetchSpy).not.toHaveBeenCalled();
   });
 });
