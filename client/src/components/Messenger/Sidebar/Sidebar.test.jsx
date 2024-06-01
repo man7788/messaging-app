7ba36ead7f98b16b089500afa514f8cd9563ad9a
man7788 <jerman1841@gmail.com>
2024-06-02 07:12:42 +0800

    Add matchMedia mock

--- client/src/components/Messenger/Sidebar/Sidebar.test.jsx
+++ client/src/components/Messenger/Sidebar/Sidebar.test.jsx
@@ -25,6 +25,20 @@ vi.mock('react-router-dom', async (importOriginal) => {
   };
 });
 
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: vi.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: vi.fn(), // Deprecated
+    removeListener: vi.fn(), // Deprecated
+    addEventListener: vi.fn(),
+    removeEventListener: vi.fn(),
+    dispatchEvent: vi.fn(),
+  })),
+});
+
 const useProfilesSpy = vi.spyOn(useProfiles, 'default');
 const useFriendsSpy = vi.spyOn(useFriends, 'default');
 const useGroupsSpy = vi.spyOn(useGroups, 'default');
