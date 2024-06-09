2278a0732ad22bbb8849f7252bd2ec8846ffbe18
man7788 <jerman1841@gmail.com>
2024-06-09 10:06:59 +0800

    Update typos

--- api/models/chatModel.js
+++ api/models/chatModel.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const ChatSchema = new Schema({
-  users: [{ type: Schema.Types.ObjectId, ref: "User", require: true }],
+  users: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
 });
 
 // Export model
--- api/models/groupModel.js
+++ api/models/groupModel.js
@@ -3,8 +3,8 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const GroupSchema = new Schema({
-  name: { type: String, minLength: 1, maxLength: 50, require: true },
-  users: [{ type: Schema.Types.ObjectId, ref: "User", require: true }],
+  name: { type: String, minLength: 1, maxLength: 50, required: true },
+  users: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
 });
 
 // Export model
